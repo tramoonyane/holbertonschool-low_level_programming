db84c78188df01b30a49dd0fbad24dff28c8c47d
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-10-17 00:04:43 +0200

    Update 0-positive_or_negative.c

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -2,8 +2,19 @@
 #include <time.h>
 #include <stdio.h>
 /* more headers goes there */
-/* 0-positive_or_negative.c */
+
 /* betty style doc for function main goes there */
+/**
+ * main - Entry point of the program that checks a random number's sign.
+ *
+ * This program generates a random number and checks whether it is positive,
+ * negative, or zero. It uses the srand and rand functions from the <stdlib.h>
+ * library to generate the random number, and the time function from <time.h>
+ * to seed the random number generator. The result is then compared, and a
+ * message indicating the sign of the number is printed to the standard output.
+ *
+ * Return: Always returns 0 to indicate successful execution.
+ */
 int main(void)
 {
 	int n;
