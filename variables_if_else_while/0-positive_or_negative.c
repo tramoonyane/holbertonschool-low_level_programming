b112ab7128777accd4b1b5ef7ad1c40eac2d0bff
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-10-16 23:27:55 +0200

    Update 0-positive_or_negative.c

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -8,11 +8,11 @@ int main(void)
 {
 	int n;
 	/**
- * main - Entry point for a program that determines whether a random number is positive, negative, or zero. 
- * This program generates a random number and checks if it is positive, negative, or zero. It then prints a message
- * indicating the result.
- * Return: Always returns 0 to indicate successful execution.
- */
+         * main - Entry point for a program that determines whether a random number is positive, negative, or zero. 
+         * This program generates a random number and checks if it is positive, negative, or zero. It then prints a message
+         * indicating the result.
+         * Return: Always returns 0 to indicate successful execution.
+         */
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
