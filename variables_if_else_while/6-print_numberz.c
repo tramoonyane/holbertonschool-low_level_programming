cde83a36d62e5a46720fa9895863f5cd88449ac0
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-10-17 22:55:35 +0200

    Update 6-print_numberz.c

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,1 +1,23 @@
-#
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints single digit numbers from 0 to 9 on a single line,
+ *              followed by a newline character.
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+    int digit;
+
+    digit = '0';
+    while (digit <= '9')
+    {
+        putchar(digit);
+        digit++;
+    }
+    putchar('\n');
+
+    return (0);
+}
