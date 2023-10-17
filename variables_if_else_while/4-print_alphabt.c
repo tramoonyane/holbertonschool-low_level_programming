c3b944ee7be50a31fe246e9912606e7022aec0ab
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-10-17 21:32:27 +0200

    Update 4-print_alphabt.c

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -8,17 +8,15 @@
  */
 int main(void)
 {
-    char letter = 'a';
-
-    while (letter <= 'z')
-    {
-        if (letter != 'e' && letter != 'q')
-        {
-            putchar(letter);
-        }
-        letter++;
-    }
-
+char letter = 'a';
+while (letter <= 'z')
+{
+    if (letter != 'e' && letter != 'q')
+{
+    putchar(letter);
+}
+    letter++;
+}
     putchar('\n');
     return (0);
 }
