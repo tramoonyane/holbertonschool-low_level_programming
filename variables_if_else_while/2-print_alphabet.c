40784fe86b0014dfbfb1198881ddc71da7dd4dcc
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-10-17 20:31:24 +0200

    Update 2-print_alphabet.c

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,21 @@
-#
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints the lowercase alphabet using putchar
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+
+	return (0);
+}
