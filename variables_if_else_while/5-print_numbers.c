b18a274c5669cd04a0cc51c0e4665450a609c387
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-10-17 22:17:46 +0200

    Update 5-print_numbers.c

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -10,17 +10,14 @@
  */
 int main(void)
 {
-    int i;
-
-    for (i = 0; i <= 9; i++)
-    {
-        printf("%d", i);
-
-        if (i < 9)
-            printf(" ");
-        else
-            printf("\n");
-    }
-
-    return (0);
+int i;
+for (i = 0; i <= 9; i++)
+{
+printf("%d", i);
+if (i < 9)
+printf(" ");
+else
+printf("\n");
+}
+return (0);
 }
