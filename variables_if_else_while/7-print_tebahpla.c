3804e2b99e36bfd5d7410fe2bda5abdb7bcd9815
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-10-17 23:30:09 +0200

    Update 7-print_tebahpla.c

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -11,10 +11,10 @@
 int main(void)
 {
 char letter = 'z';
-while (letter >= 'a')  
+while (letter >= 'a')
 {
-putchar(letter);  
-letter--;         
+putchar(letter);
+letter--;
 }
 putchar('\n');
 return (0);
