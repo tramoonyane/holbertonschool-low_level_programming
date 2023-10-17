91c2f0307ca40f8263b9cbaea3c3f828eede532c
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-10-17 18:52:25 +0200

    Update 1-last_digit.c

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ n = rand() - RAND_MAX / 2;
 m = n % 10;
 if (m > 5)
 {
-printf("Last digit of %d is %d and is greater than 5\n", n, m);
+printf("Last digit of %d is %d and is greater than 5\r\n", n, m);
 }
 else if (m < 6 && m != 0)
 {
