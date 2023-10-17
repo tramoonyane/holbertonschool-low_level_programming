82607468d2f70ba91f61b02f95cfd8b2889d951e
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-10-17 23:41:19 +0200

    Update 8-print_base16.c

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,1 +1,24 @@
-#
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints all lowercase hexadecimal digits (0-9, a-f)
+ * followed by a newline character using the putchar function only three times.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+		putchar(ch);
+
+	for (ch = 'a'; ch <= 'f'; ch++)
+		putchar(ch);
+
+	putchar('\n');
+
+	return (0);
+}
