3cb91b091cfa5b95ed0b7038882a9ef67c2de714
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-10-17 16:31:30 +0200

    Update 1-last_digit.c

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -5,9 +5,13 @@
 
 /* betty style doc for function main goes there */
 /**
-*main -This is a function
-*Return number generated
-*/
+ * main - Entry point
+ *
+ * Description: This program generates a random number and
+ * determines the last digit of that number.
+ *
+ * Return: 0 (Success)
+ */
 int main(void)
 {
 	int n;
