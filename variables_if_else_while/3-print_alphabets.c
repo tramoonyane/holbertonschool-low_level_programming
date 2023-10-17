a4213fac3ae86c4fd898ee3e944e8503dc08b83e
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-10-17 20:44:14 +0200

    Update 3-print_alphabets.c

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,1 +1,26 @@
-#
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints the alphabet in lowercase, then in uppercase, followed by a newline.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+ /* Print lowercase alphabet */
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+/* Print uppercase alphabet */
+	for (letter = 'A'; letter <= 'Z'; letter++)
+	{
+		putchar(letter);
+	}
+
+	putchar('\n'); // Print a newline character
+
+	return (0);
+}
