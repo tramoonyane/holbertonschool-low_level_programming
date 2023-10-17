0779725d52353caf4c57f7d3d881b273be241b49
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-10-17 23:19:59 +0200

    Update 7-print_tebahpla.c

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,1 +1,22 @@
-#
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print the lowercase alphabet in reverse on a single line
+ * without spaces using putchar function.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter = 'z';  
+
+	while (letter >= 'a')  
+	{
+		putchar(letter);  
+		letter--;         
+	}
+	putchar('\n'); 
+	return (0);
+}
