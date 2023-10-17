048b971ccbb97ac5d1b0972950126848cb3deb23
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-10-17 22:12:47 +0200

    Update 5-print_numbers.c

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,19 +1,26 @@
 #include <stdio.h>
+
 /**
  * main - Entry point of the program
  *
- * This function prints all single-digit numbers of base 10
- * starting from 0, each followed by a new line.
+ * This program prints all single-digit numbers of base 10
+ * starting from 0 in a single line, followed by a new line.
  *
  * Return: 0 (Success)
  */
 int main(void)
 {
-int number = 0;
-while (number < 10)
-{
-printf("%d", number);
-number++;
-}
-return (0);
+    int i;
+
+    for (i = 0; i <= 9; i++)
+    {
+        printf("%d", i);
+
+        if (i < 9)
+            printf(" ");
+        else
+            printf("\n");
+    }
+
+    return (0);
 }
