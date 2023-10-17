213a01ccbec5507fbe4bc456c011d79ae0520c8e
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-10-17 23:51:56 +0200

    Update 9-print_comb.c

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,1 +1,29 @@
-#
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all possible combinations of single-digit numbers
+ * separated by ', ' in ascending order using putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int digit;
+
+    for (digit = 0; digit <= 9; digit++)
+    {
+        putchar(digit + '0'); // Convert digit to character and print
+
+        if (digit < 9)
+        {
+            putchar(','); // Print comma
+            putchar(' '); // Print space
+        }
+    }
+
+    putchar('\n'); // Print a newline character
+
+    return (0);
+}
