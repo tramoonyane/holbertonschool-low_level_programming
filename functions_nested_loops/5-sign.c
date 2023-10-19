e7573819afbab1f7ca87896eae83458383898f46
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-10-19 16:19:09 +0200

    function that prints the sign of a number

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,9 +1,10 @@
 #include "main.h"
+
 /**
- * print_sign -print the sign of a number
- * @n: The number to be checked
- * Return: 1 for the positive num, -1 for negative num or zero for anything else
- * */
+* print_sign -print the sign of a number
+* @n: The number to be checked
+* Return: 1 for the positive num, -1 for negative num or zero for anything else
+*/
 
 int print_sign(int n)
 {
