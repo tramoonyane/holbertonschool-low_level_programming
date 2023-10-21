6cb1060b88fb2a0dcbb420ff05f9bf3e77836870
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-10-22 01:39:54 +0200

    Update 7-print_diagonal.c

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,1 +1,24 @@
-3
+#include "main.h"
+
+/**
+*  print_diagonal  -  printing lines  diagonal
+*  @n:  input variable
+*
+*  Return:  void
+*/
+
+void print_diagonal(int n)
+{
+int i, j;
+if (n <= 0)
+_putchar('\n');
+for (i = 0; i < n; i++)
+{
+for (j = 0; j < i; j++)
+{
+_putchar('_');
+}
+_putchar('\\');
+_putchar('\n');
+}
+}
