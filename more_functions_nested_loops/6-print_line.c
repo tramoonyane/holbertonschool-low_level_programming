26e83f5524468384c1fa8a521a712d2c070f4fcb
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-10-22 01:22:17 +0200

    Update 6-print_line.c

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,1 +1,17 @@
-3
+#include "main.h"
+
+/**
+*  print_line  -straight  -line
+*  @n:Number of times
+*  Return:Always 0
+*/
+
+void print_line(int n)
+{
+int i;
+for (i = 0; i <n; i++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
