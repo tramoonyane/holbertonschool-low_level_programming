e63437e2066a207bc80100c4536f0a2da6cd3f2b
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-10-22 06:21:52 +0200

    Update 100-prime_factor.c

--- more_functions_nested_loops/100-prime_factor.c
+++ more_functions_nested_loops/100-prime_factor.c
@@ -9,9 +9,9 @@
 int main(void)
 {
 unsigned long int i, n = 612852475143;
-for (i = 3; i < 782849; i + 2)
+for (i = 3; i < 782849; i += 2)
 {
-while ((n % i = 0) && (n != i))
+while ((n % i == 0) && (n != i))
 n = n / i;
 }
 printf("%lu\n", n);
