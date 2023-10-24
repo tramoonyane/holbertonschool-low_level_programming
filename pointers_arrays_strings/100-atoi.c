a2a732dc1d1dc0c4a83426d1e7926f73b3898356
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-10-24 23:05:30 +0200

    Update 100-atoi.c

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,40 +1,40 @@
 #include "main.h"
 /**
-* _atoi - converts a string to an integer
-* @s: string to be converted
+* _atoi - Converts a string to an integer.
+* @s: The input string to convert.
 *
-* Return: the int converted from the string
+* Return: The integer value of the string.
 */
 int _atoi(char *s)
 {
-int i, d, n, len, f, digit;
-i = 0;
-d = 0;
-n = 0;
-len = 0;
-f = 0;
-digit = 0;
-while (s[len] != '\0')
-len++;
-while (i < len && f == 0)
+int index = 0;
+int negative = 0;
+int result = 0;
+int foundDigit = 0;
+int length = 0;
+while (s[length] != '\0')
+length++;
+  
+while (index < length && foundDigit == 0)
 {
-if (s[i] == '-')
-++d;
-
-if (s[i] >= '0' && s[i] <= '9')
+if (s[index] == '-')
+negative++;
+if (s[index] >= '0' && s[index] <= '9')
 {
-digit = s[i] - '0';
-if (d % 2)
+int digit = s[index] - '0';
+if (negative % 2)
 digit = -digit;
-n = n * 10 + digit;
-f = 1;
-if (s[i + 1] < '0' || s[i + 1] > '9')
+result = result * 10 + digit;
+foundDigit = 1;
+
+if (s[index + 1] < '0' || s[index + 1] > '9')
 break;
-f = 0;
+foundDigit = 0;
 }
-i++;
+index++;
 }
-if (f == 0)
+
+if (foundDigit == 0)
 return (0);
-return (n);
+return (result);
 }
