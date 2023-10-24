a463d9c14c39c5b2a58ef562d92def894129ec7c
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-10-24 20:56:39 +0200

    Update 9-strcpy.c

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,7 +1,6 @@
 #include "main.h"
-
 /**
-* char *_strcpy - a function that copies the string pointed to by src
+* _strcpy - a function that copies the string pointed to by src
 * @dest: copy to
 * @src: copy from
 * Return: string
@@ -9,15 +8,16 @@
 char *_strcpy(char *dest, char *src)
 {
 int l = 0;
-int x = 0;
-while (*(src + l) != '\0')
+while (src[l] != '\0')
 {
 l++;
 }
-for ( ; x < l ; x++)
+int x = 0;
+while (x < l)
 {
 dest[x] = src[x];
+x++;
 }
 dest[l] = '\0';
-return (dest);
+return dest;
 }
