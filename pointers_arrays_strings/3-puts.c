defdc9cff7fa4162c096ed85854d65f2374eeb27
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-10-24 03:30:06 +0200

    Update 3-puts.c

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,1 +1,23 @@
-#
+#include "main.h"
+
+/**
+* _puts - Prints a string followed by a new line to stdout.
+*
+* @str: The string to be printed.
+*
+* Description:
+* This function prints the characters of a string to the standard output
+* (stdout) followed by a newline character ('\n') to create a new line.
+*
+* Return: No return value.
+*/
+
+#include "main.h"
+void _puts(char *str)
+{
+while (*str != '\0')
+{
+_putchar(*str++);
+}
+_putchar('\n');
+}
