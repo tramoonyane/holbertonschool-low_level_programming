10d41fab4ff8ac23da35dccf1a3fce7f98f4c26f
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-10-25 15:14:07 +0200

    Update 2-strncpy.c

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
-* _strncat - Concatenates two strings, using at most n bytes from src.
+* _strncpy - Copies at most n bytes from the source string to the destination string.
 * @dest: The destination string.
-* @src: The source string to be appended.
-* @n: The maximum number of bytes to use from src.
+* @src: The source string to be copied from.
+* @n: The maximum number of bytes to copy from src.
 *
-* Return: A pointer to the resulting concatenated string (dest).
+* Return: A pointer to the resulting copied string (dest).
 */
 char *_strncpy(char *dest, char *src, int n)
 {
