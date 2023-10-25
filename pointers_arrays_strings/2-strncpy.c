58ab38feb05ad0a39735d29d10fce12f95415c9a
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-10-25 14:58:56 +0200

    Update 2-strncpy.c

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -7,7 +7,7 @@
 *
 * Return: A pointer to the resulting concatenated string (dest).
 */
-char *_strncat(char *dest, const char *src, int n)
+char *_strncpy(char *dest, char *src, int n);
 {
 char *dest_ptr = dest;
 for (; *dest_ptr != '\0'; dest_ptr++)
