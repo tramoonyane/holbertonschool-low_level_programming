4b6775a782f873ffe4ea3d0d6442f50f93ab54f1
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-10-25 23:07:14 +0200

    Update 4-rev_array.c

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -1,25 +1,22 @@
 /**
- * reverse_array - Reverses the content of an array of integers.
- * @a: Pointer to the array of integers.
- * @n: Number of elements in the array.
- *
- * Description: This function takes an array of integers and its length
- * as arguments and reverses the order of the elements in the array.
- */
+* reverse_array - Reverses the content of an array of integers.
+* @a: Pointer to the array of integers.
+* @n: Number of elements in the array.
+*
+* Description: This function takes an array of integers and its length
+* as arguments and reverses the order of the elements in the array.
+*/
 void reverse_array(int *a, int n)
 {
-  int start = 0;
-  int end = n - 1;
-  int temp;
-
-  while (start < end)
-    {
-      /* Swap elements at the start and end positions */
-      temp = a[start];
-      a[start] = a[end];
-      a[end] = temp;
-
-      start++;
-      end--;
-    }
+int start = 0;
+int end = n - 1;
+int temp;
+while (start < end)
+{
+temp = a[start];
+a[start] = a[end];
+a[end] = temp;
+start++;
+end--;
+}
 }
