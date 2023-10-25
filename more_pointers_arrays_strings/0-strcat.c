a912e4f9d0c16b55a7f2e8206068b097d89292c4
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-10-25 04:38:45 +0200

    Update 0-strcat.c

--- more_pointers_arrays_strings/0-strcat.c
+++ more_pointers_arrays_strings/0-strcat.c
@@ -1,27 +1,22 @@
 /**
- * _strcat - Concatenates two strings.
- * @dest: The destination string to which src will be appended.
- * @src: The source string that will be appended to dest.
- *
- * Return: A pointer to the resulting string (dest).
- */
-char *_strcat(char *dest, char *src) {
-    char *originalDest = dest;
-
-    // Move the dest pointer to the end of the string
-    while (*dest != '\0') {
-        dest++;
-    }
-
-    // Copy the characters from src to dest
-    while (*src != '\0') {
-        *dest = *src;
-        dest++;
-        src++;
-    }
-
-    // Add a null-terminating character at the end
-    *dest = '\0';
-
-    return originalDest; // Return a pointer to the resulting string
+* _strcat - Concatenates two strings.
+* @dest: The destination string to which src will be appended.
+* @src: The source string that will be appended to dest.
+*
+* Return: A pointer to the resulting string (dest).
+*/
+char *_strcat(char *dest, char *src)
+{
+char *originalDest = dest;
+while (*dest != '\0') {
+dest++;
+}
+while (*src != '\0')
+{
+*dest = *src;
+dest++;
+src++;
+}
+*dest = '\0';
+return (originalDest);
 }
