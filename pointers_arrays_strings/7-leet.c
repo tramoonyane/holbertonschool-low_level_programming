79a016463a44b1d66a5c1eb4aa2f378fef9d1625
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-10-26 01:18:31 +0200

    Update 7-leet.c

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,26 +1,24 @@
 /**
- * leet - Encodes a string into "1337".
- * @str: The input string.
- *
- * Return: Pointer to the modified string.
- */
+* leet - Encodes a string into "1337".
+* @str: The input string.
+*
+* Return: Pointer to the modified string.
+*/
 char *leet(char *str)
 {
-  char *leet_dict = "aAeEoOtTlL";
-  char *leet_codes = "44370171";
-  int i, j;
-
-  for (i = 0; str[i] != '\0'; i++)
-    {
-      for (j = 0; leet_dict[j] != '\0'; j++)
-        {
-	  if (str[i] == leet_dict[j])
-            {
-	      str[i] = leet_codes[j];
-	      break;
-            }
-        }
-    }
-
-  return str;
+char *leet_dict = "aAeEoOtTlL";
+char *leet_codes = "44370171";
+int i, j;
+for (i = 0; str[i] != '\0'; i++)
+{
+for (j = 0; leet_dict[j] != '\0'; j++)
+{
+if (str[i] == leet_dict[j])
+{
+str[i] = leet_codes[j];
+break;
+}
+}
+}
+return (str);
 }
