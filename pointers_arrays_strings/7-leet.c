d2552110c76985f9ec2ce67c9e1aff6d98a9b184
tramoonyane <theteleramoonyane5@gmail.com>
2023-10-25 16:10:43 -0700

    save 7-leet.c

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+/**
+ * leet - Encodes a string into "1337".
+ * @str: The input string.
+ *
+ * Return: Pointer to the modified string.
+ */
+char *leet(char *str)
+{
+  char *leet_dict = "aAeEoOtTlL";
+  char *leet_codes = "44370171";
+  int i, j;
+
+  for (i = 0; str[i] != '\0'; i++)
+    {
+      for (j = 0; leet_dict[j] != '\0'; j++)
+        {
+	  if (str[i] == leet_dict[j])
+            {
+	      str[i] = leet_codes[j];
+	      break;
+            }
+        }
+    }
+
+  return str;
+}
