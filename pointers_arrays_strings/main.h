6d0b7d81db2ecee9ce260ec3aaba2a231d6eb303
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-10-25 16:29:12 +0200

    Update main.h

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -15,5 +15,6 @@ int _atoi(char *s);
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
 
 #endif
