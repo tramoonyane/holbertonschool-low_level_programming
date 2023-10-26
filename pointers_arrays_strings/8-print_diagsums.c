8862c7dad2bb718fd7883f9800001af8c4743a7e
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-10-27 00:41:37 +0200

    Update 8-print_diagsums.c

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -23,6 +23,6 @@ for (i = 0; i < size; i++)
 main_diag_sum += a[i * size + i];
 secondary_diag_sum += a[i * size + (size - 1 - i)];
 }
-printf("Sum of the main diagonal: %d\n", main_diag_sum);
-printf("Sum of the secondary diagonal: %d\n", secondary_diag_sum);
+printf(%d\n", main_diag_sum);
+printf(%d\n", secondary_diag_sum);
 }
