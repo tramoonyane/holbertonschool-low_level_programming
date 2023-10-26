c9f20e5f103d51a2de47a273f9674f39996f4cb4
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-10-26 17:41:04 +0200

    Update 7-print_chessboard.c

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,1 +1,20 @@
-#
+/**
+* print_chessboard - Print a chessboard
+* @a: The 2D array representing the chessboard
+*
+* This function prints a chessboard represented by a 2D array of characters.
+*
+* Return: void (no return value)
+*/
+void print_chessboard(char (*a)[8])
+{
+int row, col;
+for (row = 0; row < 8; row++)
+{
+for (col = 0; col < 8; col++)
+{
+_putchar(a[row][col]);
+}
+_putchar('\n');
+}
+}
