7c8a2c6eae0dbea56203d06200df6a2c8a0bad90
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-10-26 14:05:22 +0200

    Update 1-memcpy.c

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,1 +1,25 @@
-#
+#include "main.h"
+/**
+* _memcpy - Copy memory area
+* @dest: Destination memory area
+* @src: Source memory area
+* @n: Number of bytes to copy
+*
+* This function copies 'n' bytes from the source memory area 'src' to the
+* destination memory area 'dest'. It does not check for buffer overflow,
+* so make sure 'dest' has enough space for 'n' bytes.
+*
+* Return: A pointer to the destination memory area 'dest'.
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+char *original_dest = dest;
+while (n > 0)
+{
+*dest = *src;
+dest++;
+src++;
+n--;
+}
+return (original_dest);
+}
