c988183a124e7d8ef6b97984440da1799c790f1e
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-10-27 01:40:35 +0200

    Update 8-print_diagsums.c

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -20,9 +20,14 @@ int secondary_diag_sum = 0;
 int i;
 for (i = 0; i < size; i++)
 {
-main_diag_sum += a[i * size + i];
-secondary_diag_sum += a[i * size + (size - 1 - i)];
+main_diag_sum += a[i];
+a += size;
 }
-printf("%d\n", main_diag_sum);
-printf("%d\n", secondary_diag_sum);
+a -= size;
+for (i = 0; i < size; i++)
+{
+secondary_diag_sum += a[i];
+a -= size;
 }
+printf("%d, %d\n", main_diag_sum, secondary_diag_sum);
+} 
