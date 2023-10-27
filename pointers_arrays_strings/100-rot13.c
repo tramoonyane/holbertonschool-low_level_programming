a6c51385b3cd20da0b4932a5ab3720ff85e532a4
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-10-27 22:49:32 +0200

    Update 100-rot13.c

--- pointers_arrays_strings/100-rot13.c
+++ pointers_arrays_strings/100-rot13.c
@@ -1,1 +1,26 @@
 #include "main.h"
+/**
+ * rot13 - Encodes a string using the ROT13 cipher.
+ * @str: The input string to be encoded.
+ *
+ * Return: A pointer to the encoded string.
+ */
+char *rot13(char *str) {
+    if (str == NULL)
+        return NULL;
+
+    char *ptr = str;
+
+    while (*ptr) {
+        char c = *ptr;
+        
+        if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')) {
+            char base = (c >= 'A' && c <= 'Z') ? 'A' : 'a';
+            *ptr = (c - base + 13) % 26 + base;
+        }
+
+        ptr++;
+    }
+
+    return str;
+}
