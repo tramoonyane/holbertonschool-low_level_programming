18d87ad814d7b3935c570571dd2bae754a9d5e96
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-10-28 00:54:26 +0200

    Update 101-print_number.c

--- pointers_arrays_strings/101-print_number.c
+++ pointers_arrays_strings/101-print_number.c
@@ -1,20 +1,32 @@
-include "main.h"
-#include <unistd.h>
+#include "main.h"
 
 /**
 * print_number - Prints an integer.
 * @n: The integer to be printed.
 */
+
 void print_number(int n)
 {
+int divisor = 1;
+int is_negative = 0;
 if (n < 0)
 {
 _putchar('-');
+is_negative = 1;
 n = -n;
 }
-if (n / 10 != 0)
+while (n / divisor >= 10)
 {
-print_number(n / 10);
+divisor *= 10;
 }
-_putchar((n % 10) + '0');
+while (divisor > 0)
+{
+_putchar((n / divisor) + '0');
+n %= divisor;
+divisor /= 10;
+}
+if (is_negative == 0 && n == 0)
+{
+_putchar('0');
+}
 }
