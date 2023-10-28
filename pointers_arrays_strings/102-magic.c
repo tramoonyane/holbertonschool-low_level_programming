a6ddbf18fb91728c6639c392b02ae33fe4c38c3b
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-10-28 03:42:51 +0200

    Update 102-magic.c

--- pointers_arrays_strings/102-magic.c
+++ pointers_arrays_strings/102-magic.c
@@ -7,14 +7,14 @@ int main(void)
 
 	p = a;
 
-	/* Not allowed to use the variable a
-	* Not allowed to modify the variable p
-	* Only write one statement
-	* Not allowed to use ,
-	* Not allowed to code anything else
-	* than the line of expected line of code
-	* at the expected line before the ;
-	*/
+/* 
+ * Not allowed to use the variable a
+ * Not allowed to modify the variable p
+ * Only write one statement
+ * Not allowed to use ,
+ * Not allowed to code anything else
+ * than the line of expected line of code at the expected line before the ;
+ */
 	*p = (int[]){98};
 	/* Add one line here to print "a[2] = 98," followed by a new line */
 	printf("a[2] = %d\n", 98);
