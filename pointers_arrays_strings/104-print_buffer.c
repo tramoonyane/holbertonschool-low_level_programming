a37060f31fda33dd9f66103affdb38d4f1ca8750
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-10-28 06:27:52 +0200

    Update 104-print_buffer.c

--- pointers_arrays_strings/104-print_buffer.c
+++ pointers_arrays_strings/104-print_buffer.c
@@ -18,35 +18,31 @@
  */
 void print_buffer(char *b, int size)
 {
-    if (size <= 0)
-    {
-        printf("\n");
-        return;
-    }
-
-    for (int i = 0; i < size; i += 10)
-    {
-        printf("%08x: ", i);
-        for (int j = 0; j < 10; j++)
-        {
-            if (i + j < size)
-                printf("%02x%c", b[i + j], j % 2 == 1 ? ' ' : '\0');
-            else
-                printf("  ");
-        }
-
-        printf(" ");
-
-        for (int j = 0; j < 10; j++)
-        {
-            if (i + j < size)
-                printf("%c", (b[i + j] >= 32 && b[i + j] <= 126) ? b[i + j] : '.');
-            else
-                printf(" ");
-        }
-
-        printf("\n");
-    }
+if (size <= 0)
+{
+printf("\n");
+return;
+}
+for (int i = 0; i < size; i += 10)
+{
+printf("%08x: ", i);
+for (int j = 0; j < 10; j++)
+{
+if (i + j < size)
+printf("%02x%c", b[i + j], j % 2 == 1 ? ' ' : '\0');
+else
+printf("  ");
+}
+printf(" ");
+for (int j = 0; j < 10; j++)
+{
+if (i + j < size)
+printf("%c", (b[i + j] >= 32 && b[i + j] <= 126) ? b[i + j] : '.');
+else
+printf(" ");
+}
+printf("\n");
+}
 }
 
 /**
@@ -61,8 +57,8 @@ void print_buffer(char *b, int size)
  */
 int main(void)
 {
-    char buffer[] = "This is a string!\nAnd this is the rest of the #buffer :)";
-    int size = sizeof(buffer) - 1;
-    print_buffer(buffer, size);
-    return (0);
+char buffer[] = "This is a string!\nAnd this is the rest of the #buffer :)";
+int size = sizeof(buffer) - 1;
+print_buffer(buffer, size);
+return (0);
 }
