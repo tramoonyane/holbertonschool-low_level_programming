2e4d7bd0fd69f3e21f71ea5609dce47ea9ad85d6
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-10-31 00:57:10 +0200

    Update 3-factorial.c

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -1,1 +1,15 @@
-#
+#include "main.h"
+
+/**
+* factorial - Calculate the factorial of a number recursively
+* @n: The input number
+* Return: The factorial of n, or -1 for error
+*/
+int factorial(int n)
+{
+if (n < 0)
+return -1;
+if (n == 0 || n == 1)
+return 1;
+return n * factorial(n - 1);
+}
