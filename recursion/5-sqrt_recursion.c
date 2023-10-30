e7e698391fb4d0a2d068d060e61cf9ce7f552822
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-10-31 01:21:25 +0200

    Update 5-sqrt_recursion.c

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,1 +1,15 @@
-#
+#include "main.h"
+
+/**
+* _sqrt_recursion - Calculate the natural square root of a number recursively
+* @n: The input number
+* Return: The natural square root of n, or -1 for error
+*/
+int _sqrt_recursion(int n)
+{
+if (n < 0)
+return (-1);
+if (n == 0 || n == 1)
+return (n);
+return _sqrt_helper(n, 1);
+}
