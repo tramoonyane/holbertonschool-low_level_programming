e3345ebed32a52465f7546772d48b818f19e383c
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-10-31 01:36:52 +0200

    Update main.h

--- recursion/main.h
+++ recursion/main.h
@@ -6,6 +6,7 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
+int _sqrt_helper(int n, int guess);
 int is_prime_number(int n);
 int _putchar(char c);
 
