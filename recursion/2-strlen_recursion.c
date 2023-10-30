4d7df1b0a0bac260cc0af02bccecf95c7f3285b6
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-10-31 00:46:11 +0200

    Update 2-strlen_recursion.c

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -8,6 +8,6 @@
 int _strlen_recursion(char *s)
 {
 if (*s == '\0') // Base case: end of string
-return 0;
-return 1 + _strlen_recursion(s + 1);
+return (0);
+return (1 + _strlen_recursion(s + 1));
 }
