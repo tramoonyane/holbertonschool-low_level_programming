27fc53ab5720fa626a4a9f132b2974443397d0a8
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-10-31 01:29:57 +0200

    Update 5-sqrt_recursion.c

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -13,3 +13,18 @@ if (n == 0 || n == 1)
 return (n);
 return (_sqrt_helper(n, 1));
 }
+
+/**
+* _sqrt_helper - Helper function to find the natural square root recursively
+* @n: The input number
+* @guess: The current guess for the square root
+* Return: The natural square root of n, or -1 for error
+*/
+int _sqrt_helper(int n, int guess)
+{
+if (guess * guess == n)
+return (guess);
+if (guess * guess > n)
+return (-1);
+return (_sqrt_helper(n, guess + 1));
+}
