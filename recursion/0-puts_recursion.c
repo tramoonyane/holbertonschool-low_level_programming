cb8155b2b0981ddfb009fe8a183cf413bf3532a3
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-10-30 22:59:18 +0200

    Update 0-puts_recursion.c

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,1 +1,17 @@
-#
+#include main.h
+/**
+ * _puts_recursion - Print a string followed by a newline character recursively
+ * @s: The string to be printed
+ */
+
+void _puts_recursion(char *s)
+{
+    if (*s == '\0')
+    {
+        _putchar('\n');
+        return;
+    }
+
+    _putchar(*s); // Print the current character
+    _puts_recursion(s + 1);
+}
