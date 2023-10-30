4e105784c7182893cedbd14558118b6715c612b8
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-10-31 01:09:19 +0200

    Update 4-pow_recursion.c

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,1 +1,16 @@
-#
+#include "main.h"
+
+/**
+* _pow_recursion - Calculate the value of x raised to the power of y recursively
+* @x: The base
+* @y: The exponent
+* Return: The result of x^y, or -1 for error
+*/
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+return -1;
+if (y == 0)
+return (1);
+return (x * _pow_recursion(x, y - 1));
+}
