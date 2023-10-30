b89ca31b5b8b3af35726eada5677965c3d2789c3
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-10-31 00:36:59 +0200

    Update 2-strlen_recursion.c

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,1 +1,13 @@
-#
+#include "main.h"
+
+/**
+* _strlen_recursion - Calculate the length of a string recursively
+* @s: The input string
+* Return: The length of the string
+*/
+int _strlen_recursion(char *s)
+{
+if (*s == '\0') // Base case: end of string
+return 0;
+return 1 + _strlen_recursion(s + 1);
+}
