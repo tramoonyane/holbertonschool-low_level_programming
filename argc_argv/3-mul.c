1d6fd9e36f95e6ce4bd1a356e5e607560a4b7f7d
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-11-01 16:07:49 +0200

    Update 3-mul.c

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -16,9 +16,9 @@ if (argc != 3)
 printf("Error\n");
 return (1);
 }
-int num1 = atoi(argv[1]);
-int num2 = atoi(argv[2);
-int result = num1 * num2;
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2);
+result = num1 * num2;
 printf("%d\n", result);
 return (0);
 }
