95d9a059ba663a42c4491d4b1919937fe6a467ab
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-11-01 15:36:41 +0200

    Update 2-args.c

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,1 +1,18 @@
-#
+#include <stdio.h>
+
+/**
+* main - Entry point of the program
+* @argc: The number of command line arguments (including the program name)
+* @argv: An array of strings containing the command line arguments
+*
+* Return: 0 on success
+*/
+int main(int argc, char *argv[])
+{
+int i;
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
+return (0);
+}
