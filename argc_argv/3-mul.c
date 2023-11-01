bf110ed226d7f8f0aaaf5caef02182d8824818e6
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-11-01 15:47:21 +0200

    Update 3-mul.c

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,1 +1,23 @@
-#
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - Entry point of the program
+* @argc: The number of command line arguments (including the program name)
+* @argv: An array of strings containing the command line arguments
+*
+* Return: 0 on success, 1 on error
+*/
+int main(int argc, char *argv[])
+{
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+}
+int num1 = atoi(argv[1]);
+int num2 = atoi(argv[2);
+int result = num1 * num2;
+printf("%d\n", result);
+return (0);
+}
