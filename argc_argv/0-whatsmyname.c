90329065244c3487e2d08b5ae9997e18df726f2c
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-11-01 14:14:29 +0200

    Update 0-whatsmyname.c

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,1 +1,22 @@
-@##
+#include <stdio.h>
+
+/**
+* main - Entry point of the program
+* @argc: The number of command line arguments (including the program name)
+* @argv: An array of strings containing the command line arguments
+*
+* Return: 0 on success, 1 on failure
+*/
+int main(int argc, char *argv[])
+{
+if (argc > 0)
+{
+printf("Program name: %s\n", argv[0]);
+}
+else
+{
+fprintf(stderr, "Unable to determine the program name.\n");
+return (1);
+}
+return (0);
+}
