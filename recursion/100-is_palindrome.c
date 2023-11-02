a4afa567654633961e6b3168a215067653c9c829
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-11-02 13:05:50 +0200

    Update 100-is_palindrome.c

--- recursion/100-is_palindrome.c
+++ recursion/100-is_palindrome.c
@@ -1,1 +1,28 @@
-#
+/**
+* is_palindrome - Check if a string is a palindrome.
+* @s: The string to check.
+*
+* Return: 1 if the string is a palindrome, 0 otherwise.
+*/
+int is_palindrome(char *s)
+{
+if (*s == '\0' || *s == '\n')
+return (1);
+static int start = 0;
+static int end;
+if (start == 0)
+end = strlen(s) - 1;
+if (start >= end)
+{
+start = 0;
+return (1);
+}
+if (s[start] != s[end])
+{
+start = 0;
+return (0);
+}
+start++;
+end--;
+return (is_palindrome(s));
+}
