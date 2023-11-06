816fbe91eb4ed66604601b665f8612b239c7ef97
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-11-06 23:30:18 +0200

    Update 3-array_range.c

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -14,7 +14,7 @@ int size, i;
 if (min > max)
 return (NULL);
 size = max - min + 1;
-arr = malloc(size * sizeof(int));
+arr = malloc(size *sizeof(int));
 if (arr == NULL)
 return (NULL);
 for (i = 0; i < size; i++)
