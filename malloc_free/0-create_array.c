d923c349ce8ad6d5e8d24a484d6d3171a84ed35e
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-11-06 15:29:48 +0200

    Update 0-create_array.c

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,1 +1,49 @@
-#
+#include <stdlib.h>
+
+/**
+* _putchar - Custom function to print a character
+* @c: The character to print
+*/
+void _putchar(char c)
+{
+
+}
+
+/**
+* create_array - Create an array of characters and initialize it with a specific character
+* @size: The size of the array
+* @c: The character to initialize the array with
+* Return: Pointer to the created array, or NULL on failure
+*/
+char *create_array(unsigned int size, char c)
+{
+if (size == 0)
+{
+return (NULL);
+}
+char *array = (char *)malloc(size * sizeof(char));
+if (array == NULL)
+{
+return (NULL);
+}
+for (unsigned int i = 0; i < size; i++)
+{
+array[i] = c;
+}
+return array;
+}
+int main()
+{
+unsigned int size = 10;
+char fill_char = 'A';
+char *result = create_array(size, fill_char);
+if (result != NULL)
+{
+for (unsigned int i = 0; i < size; i++)
+{
+_putchar(result[i]);
+}
+free(result);
+}
+return (0);
+}
