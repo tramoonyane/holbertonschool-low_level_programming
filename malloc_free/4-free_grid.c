c7654e5d95fa410be088f590204c30fa9190be1e
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-11-06 19:12:49 +0200

    Update 4-free_grid.c

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -1,1 +1,23 @@
-#
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* free_grid - Frees the memory of a 2D grid previously created by alloc_grid.
+* @grid: The 2D grid to be freed.
+* @height: The height of the grid.
+*/
+void free_grid(int **grid, int height)
+{
+int i;
+if (grid == NULL)
+return;
+for (i = 0; i < height; i++)
+{
+if (grid[i] != NULL)
+{
+free(grid[i]);
+grid[i] = NULL;
+}
+}
+free(grid);
+}
