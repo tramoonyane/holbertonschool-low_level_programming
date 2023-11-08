bf60ba36423f03aafdab7041c06effe6ede223bb
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-11-08 22:39:04 +0200

    Update 101-strtow.c

--- malloc_free/101-strtow.c
+++ malloc_free/101-strtow.c
@@ -1,22 +1,7 @@
 #include "main.h"
 #include <unistd.h>
 #include <stdlib.h>
-/**
- * word_count - Counts the number of words in a string.
- * @str: The string to count the words in.
- * Return: The number of words in the string.
- */
-int word_count(char *str)
-{
-int count = 0, i = 0;
-while (str[i] != '\0')
-{
-if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
-count++;
-i++;
-}
-return (count);
-}
+
 /**
  * strtow - Splits a string into words.
  * @str: The string to split.
@@ -52,4 +37,50 @@ j++;
 }
 words[j] = NULL;
 return (words);
+}
+/**
+ * word_count - Counts the number of words in a string.
+ * @str: The string to count the words in.
+ * Return: The number of words in the string.
+ */
+int word_count(char *str)
+{
+int count = 0, i = 0;
+while (str[i] != '\0')
+{
+if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
+count++;
+i++;
+}
+return (count);
+}
+/**
+ * word_length - Calculates the length of a word starting from the given position.
+ * @str: The string to calculate the word length from.
+ * @start: The starting position of the word.
+ *
+ * Return: The length of the word.
+ */
+int word_length(char *str, int start)
+{
+int length = 0;
+int i = start;
+while (str[i] != ' ' && str[i] != '\0')
+{
+length++;
+i++;
+}
+return (length);
+}
+/**
+ * free_words_array - Frees memory allocated for each word in the words array, and the array itself.
+ * @words: The array of words to free.
+ * @size: The size of the words array.
+ */
+void free_words_array(char **words, int size)
+{
+int i = 0;
+for (i = 0; i < size; i++)
+free(words[i]);
+free(words);
 }
