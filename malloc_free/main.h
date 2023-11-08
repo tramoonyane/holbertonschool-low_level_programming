4b916650371ee877efd5af2417e7940d62f1ef29
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-11-08 19:50:48 +0200

    Update main.h

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -9,6 +9,6 @@ int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
 char **strtow(char *str);
-
+int word_count(char *str);
 
 #endif
