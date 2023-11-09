8df85ec8c4def66d7665bbe2e531041e9c111e54
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-11-09 02:34:51 +0200

    Update 101-strtow.c

--- malloc_free/101-strtow.c
+++ malloc_free/101-strtow.c
@@ -47,3 +47,57 @@ str++;
 word_array[word_index] = NULL;
 return (word_array);
 }
+/**
+ * count_words - Counts the number of words in a string.
+ * @str: The input string.
+ *
+ * Return: The number of words in the string.
+ */
+int count_words(char *str)
+{
+int count = 0;
+int in_word = 0;
+int i;
+while (*str)
+{
+if (*str == ' ')
+{
+in_word = 0;
+}
+else if (!in_word)
+{
+count++;
+in_word = 1;
+}
+str++;
+}
+return (count);
+}
+
+/**
+ * copy_word - Copies a word from the input string.
+ * @str: The input string.
+ *
+ * Return: A dynamically allocated copy of the word.
+ */
+char *copy_word(char *str)
+{
+int len = 0;
+int i;
+char *word;
+while (str[len] && str[len] != ' ')
+{
+len++;
+}
+word = (char *)malloc(len + 1);
+if (word == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < len; i++)
+{
+word[i] = str[i];
+}
+word[len] = '\0';
+return (word);
+}
