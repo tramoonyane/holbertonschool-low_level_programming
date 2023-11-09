4c875dd026019702b6eefafde45d100e7700a556
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-11-09 11:21:12 +0200

    Update 100-realloc.c

--- more_malloc_free/100-realloc.c
+++ more_malloc_free/100-realloc.c
@@ -11,44 +11,44 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-    void *new_ptr;
-    if (new_size == 0)
-    {
-        free(ptr);
-        return NULL;
-    }
-    if (ptr == NULL)
-    {
-      return malloc(new_size);
-    }
-
-    if (new_size == old_size)
-    {
-      return (ptr);
-    }
-    if (new_size > old_size)
-    {
-        new_ptr = malloc(new_size);
-        if (new_ptr == NULL)
-        {
-            return (NULL); 
-        }
-        for (unsigned int i = 0; i < old_size; i++)
-        {
-            ((char *)new_ptr)[i] = ((char *)ptr)[i];
-        }
-    } else
-    {
-        new_ptr = malloc(new_size);
-        if (new_ptr == NULL)
-        {
-            return (NULL);
-        }
-        for (unsigned int i = 0; i < new_size; i++)
-        {
-            ((char *)new_ptr)[i] = ((char *)ptr)[i];
-        }
-    }
-  free(ptr);
-  return (new_ptr);
+void *new_ptr;
+if (new_size == 0)
+{
+free(ptr);
+return NULL;
+}
+if (ptr == NULL)
+{
+return malloc(new_size);
+}
+if (new_size == old_size)
+{
+return (ptr);
+}
+if (new_size > old_size)
+{
+new_ptr = malloc(new_size);
+if (new_ptr == NULL)
+{
+return (NULL);
+}
+for (unsigned int i = 0; i < old_size; i++)
+{
+((char *)new_ptr)[i] = ((char *)ptr)[i];
+}
+}
+else
+{
+new_ptr = malloc(new_size);
+if (new_ptr == NULL)
+{
+return (NULL);
+}
+for (unsigned int i = 0; i < new_size; i++)
+{
+((char *)new_ptr)[i] = ((char *)ptr)[i];
+}
+}
+free(ptr);
+return (new_ptr);
 }
