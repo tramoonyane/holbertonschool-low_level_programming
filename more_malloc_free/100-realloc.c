7292d193552992880d1eb5fe5358208115a2ba20
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-11-10 12:06:04 +0200

    Update 100-realloc.c

--- more_malloc_free/100-realloc.c
+++ more_malloc_free/100-realloc.c
@@ -10,24 +10,27 @@
  * Return: Pointer to the newly allocated memory block
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size) {
+    void *new_ptr;
+    unsigned int i;
+    
     if (new_size == 0 && ptr != NULL) {
         free(ptr);
-        return NULL;
+        return (NULL);
     }
     if (ptr == NULL) {
-        return malloc(new_size);
+        return (malloc(new_size));
     }
     if (new_size == old_size) {
-        return ptr;
+        return (ptr);
     }
-    void *new_ptr = malloc(new_size);
+    new_ptr = malloc(new_size);
     if (new_ptr == NULL) {
-        return NULL;
+        return (NULL);
     }
     if (new_size < old_size) {
         old_size = new_size;
     }
-    for (unsigned int i = 0; i < old_size; i++) {
+    for (i = 0; i < old_size; i++) {
         ((char *)new_ptr)[i] = ((char *)ptr)[i];
     }
     free(ptr);
