603d3ea4c6044a5460088e154ca9ee219dcee7c7
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-11-11 20:53:16 +0200

    Update 101-mul.c

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -48,24 +48,81 @@ exit(98);
 }
 
 /**
- * main - multiplies two positive numbers
+ * multiply_numbers - multiplies two positive numbers represented as strings
+ * @s1: first number
+ * @s2: second number
+ * @result: array to store the result
+ * @len: length of the result array
+ */
+void multiply_numbers(char *s1, char *s2, int *result, int len)
+{
+int len1, len2, i, carry, digit1, digit2;
+len1 = _strlen(s1);
+len2 = _strlen(s2);
+
+for (i = 0; i < len; i++)
+{
+result[i] = 0;
+}
+for (len1 = len1 - 1; len1 >= 0; len1--)
+{
+digit1 = s1[len1] - '0';
+carry = 0;
+for (len2 = len2 - 1; len2 >= 0; len2--)
+{
+digit2 = s2[len2] - '0';
+carry += result[len1 + len2 + 1] + (digit1 * digit2);
+result[len1 + len2 + 1] = carry % 10;
+carry /= 10;
+}
+if (carry > 0)
+{
+result[len1 + len2 + 1] += carry;
+}
+}
+}
+
+/**
+ * print_result - prints the result array
+ * @result: array to print
+ * @len: length of the array
+ */
+void print_result(int *result, int len)
+{
+int i, a = 0;
+for (i = 0; i < len - 1; i++)
+{
+if (result[i])
+{
+a = 1;
+_putchar(result[i] + '0');
+}
+}
+if (!a)
+{
+_putchar('0');
+}
+_putchar('\n');
+}
+
+/**
+ * main - entry point of the program
  * @argc: number of arguments
  * @argv: array of arguments
+ *
  * Return: always 0 (Success)
  */
-
 int main(int argc, char *argv[])
 {
 char *s1, *s2;
-int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
-s1 = argv[1], s2 = argv[2];
+int len1, len2, len, *result;
+
 if (argc != 3 || !is_digit(argv[1]) || !is_digit(argv[2]))
 {
 errors();
 }
 s1 = argv[1];
 s2 = argv[2];
-
 len1 = _strlen(s1);
 len2 = _strlen(s2);
 len = len1 + len2 + 1;
@@ -74,32 +131,8 @@ if (!result)
 {
 return (1);
 }
-for (i = 0; i <= len1 + len2; i++)
-{
-result[i] = 0;
-}
-for (len1 = len1 - 1; len1 >= 0; len1--)
-{
-digit1 = s1[len1] - '0';
-carry = 0;
-for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
-{
-digit2 = s2[len2] - '0';
-carry += result[len1 + len2 + 1] + (digit1 *digit2);
-result[len1 + len2 + 1] = carry % 10;
-carry /= 10;
-}
-if (carry > 0)
-result[len1 + len2 + 1] += carry;
-}
-for (i = 0; i < len - 1; i++)
-if (result[i])
-a = 1;
-if (a)
-_putchar(result[i] + '0');
-if (!a)
-_putchar('0');
-_putchar('\n');
+multiply_numbers(s1, s2, result, len);
+print_result(result, len);
 free(result);
 return (0);
 }
