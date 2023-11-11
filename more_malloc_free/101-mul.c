5a9659d4e2a92cad65c95966be891b6440565981
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-11-11 11:33:03 +0200

    Update 101-mul.c

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -1,44 +1,45 @@
 #include "main.h"
-#include <unistd.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * _strlen - calculates the length of a string
- * @s: the input string
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
+ *
+ * Return: 0 if a non-digit is found, 1 otherwise
+ */
+int is_digit(char *s)
+{
+    int i = 0;
+
+    while (s[i])
+    {
+        if (s[i] < '0' || s[i] > '9')
+            return (0);
+        i++;
+    }
+    return (1);
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
  *
  * Return: the length of the string
  */
 int _strlen(char *s)
 {
-	int len = 0;
+    int i = 0;
 
-	while (s[len] != '\0')
-		len++;
-
-	return (len);
+    while (s[i] != '\0')
+    {
+        i++;
+    }
+    return (i);
 }
 
 /**
- * is_digit - checks if a string is composed of digits
- * @s: the input string
- *
- * Return: 1 if composed of digits, 0 otherwise
- */
-int is_digit(char *s)
-{
-	int i = 0;
-
-	while (s[i])
-	{
-		if (s[i] < '0' || s[i] > '9')
-			return (0);
-		i++;
-	}
-	return (1);
-}
-
-/**
- * _calloc - allocates memory for an array
+ * _calloc - allocates memory for an array and initializes to zero
  * @nmemb: number of elements
  * @size: size of each element
  *
@@ -46,20 +47,20 @@ int is_digit(char *s)
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *ptr;
-	unsigned int i;
+    char *ptr;
+    unsigned int i;
 
-	if (nmemb == 0 || size == 0)
-		return (NULL);
+    if (nmemb == 0 || size == 0)
+        return (NULL);
 
-	ptr = malloc(nmemb * size);
-	if (ptr == NULL)
-		return (NULL);
+    ptr = malloc(nmemb * size);
+    if (ptr == NULL)
+        return (NULL);
 
-	for (i = 0; i < (nmemb * size); i++)
-		ptr[i] = 0;
+    for (i = 0; i < (nmemb * size); i++)
+        ptr[i] = 0;
 
-	return (ptr);
+    return (ptr);
 }
 
 /**
@@ -67,48 +68,48 @@ void *_calloc(unsigned int nmemb, unsigned int size)
  * @argc: number of arguments
  * @argv: array of arguments
  *
- * Return: 0 on success, 98 on failure
+ * Return: always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-	int len1, len2, len_res, i, j, mul, carry = 0;
-	int *result;
+    char *s1, *s2;
+    int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
-	if (argc != 3)
-		exit(98);
-
-	len1 = _strlen(argv[1]);
-	len2 = _strlen(argv[2]);
-
-	if (!is_digit(argv[1]) || !is_digit(argv[2]))
-		exit(98);
-
-	len_res = len1 + len2;
-	result = _calloc(len_res, sizeof(*result));
-
-	for (i = len1 - 1; i >= 0; i--)
-	{
-		carry = 0;
-		for (j = len2 - 1; j >= 0; j--)
-		{
-			mul = (argv[1][i] - '0') * (argv[2][j] - '0');
-			mul += result[i + j + 1] + carry;
-			carry = mul / 10;
-			result[i + j + 1] = mul % 10;
-		}
-		result[i + j + 1] = carry;
-	}
-
-	if (result[0] == 0)
-		i = 1;
-	else
-		i = 0;
-
-	for (; i < len_res; i++)
-	{
-		_putchar(result[i] + '0');
-	}
-	_putchar('\n');
-	free(result);
-	return (0);
+    s1 = argv[1], s2 = argv[2];
+    if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+        exit(98);
+    len1 = _strlen(s1);
+    len2 = _strlen(s2);
+    len = len1 + len2 + 1;
+    result = _calloc(len, sizeof(int));
+    if (!result)
+        return (1);
+    for (i = 0; i <= len1 + len2; i++)
+        result[i] = 0;
+    for (len1 = len1 - 1; len1 >= 0; len1--)
+    {
+        digit1 = s1[len1] - '0';
+        carry = 0;
+        for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+        {
+            digit2 = s2[len2] - '0';
+            carry += result[len1 + len2 + 1] + (digit1 * digit2);
+            result[len1 + len2 + 1] = carry % 10;
+            carry /= 10;
+        }
+        if (carry > 0)
+            result[len1 + len2 + 1] += carry;
+    }
+    for (i = 0; i < len - 1; i++)
+    {
+        if (result[i])
+            a = 1;
+        if (a)
+            putchar(result[i] + '0');
+    }
+    if (!a)
+        putchar('0');
+    putchar('\n');
+    free(result);
+    return (0);
 }
