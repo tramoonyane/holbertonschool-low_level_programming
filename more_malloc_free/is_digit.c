d86c45481730caa60a13942dfa5a545392a344a3
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-11-11 21:13:24 +0200

    Update is_digit.c

--- more_malloc_free/is_digit.c
+++ more_malloc_free/is_digit.c
@@ -1,1 +1,18 @@
-#
+/**
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
+ *
+ * Return: 0 if a non-digit is found, 1 otherwise
+ */
+int is_digit(char *s)
+{
+int i = 0;
+
+while (s[i])
+{
+if (s[i] < '0' || s[i] > '9')
+return (0);
+i++;
+}
+return (1);
+}
