6e40db36f4cc572adb52bb11efd06a1ded13e7f3
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-11-11 21:35:08 +0200

    Update 101-mul.c

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -3,6 +3,25 @@
 #include <stdio.h>
 
 /**
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
+ *
+ * Return: 0 if a non-digit is found, 1 otherwise
+ */
+int is_digit(char *s)
+{
+int i = 0;
+
+while (s[i])
+{
+if (s[i] < '0' || s[i] > '9')
+return (0);
+i++;
+}
+return (1);
+}
+
+/**
  * errors - handles errors for main
  */
 void errors(void)
