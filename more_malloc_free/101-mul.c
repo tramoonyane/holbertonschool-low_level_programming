6bb7d778714792c1177a321b67b29e496d4404ed
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-11-11 13:48:12 +0200

    Update 101-mul.c

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -77,10 +77,10 @@ exit(98);
 len1 = _strlen(s1);
 len2 = _strlen(s2);
 len = len1 + len2 + 1;
-result = _calloc(len, sizeof(int));
+result = (int *)malloc(sizeof(int) * len);
 if (!result)
 return (1);
-for (i = 0; i <= len1 + len2; i++)
+for (i = 0; i <= len i++)
 result[i] = 0;
 for (len1 = len1 - 1; len1 >= 0; len1--)
 {
