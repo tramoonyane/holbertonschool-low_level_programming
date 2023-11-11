9dbd89a611abfe5849ad067fcdb4ed5cafe6b331
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-11-11 21:15:07 +0200

    Update 101-mul.c

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -3,23 +3,6 @@
 #include <stdio.h>
 
 /**
- * _strlen - returns the length of a string
- * @s: string to evaluate
- *
- * Return: the length of the string
- */
-int _strlen(char *s)
-{
-int i = 0;
-
-while (s[i] != '\0')
-{
-i++;
-}
-return (i);
-}
-
-/**
  * errors - handles errors for main
  */
 void errors(void)
