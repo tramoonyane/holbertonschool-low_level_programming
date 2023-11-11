c8f3ec0d8b791eb46dd4048238bb4d3a49277541
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-11-11 23:24:48 +0200

    Update 101-mul.c

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -1,6 +1,10 @@
+101-mul.c
+
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+
+#define ERR_MSG "Error"
 
 /**
  * is_digit - checks if a string contains a non-digit char
@@ -10,15 +14,15 @@
  */
 int is_digit(char *s)
 {
-    int i = 0;
+	int i = 0;
 
-    while (s[i])
-    {
-        if (s[i] < '0' || s[i] > '9')
-            return (0);
-        i++;
-    }
-    return (1);
+	while (s[i])
+	{
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+		i++;
+	}
+	return (1);
 }
 
 /**
@@ -29,13 +33,13 @@ int is_digit(char *s)
  */
 int _strlen(char *s)
 {
-    int i = 0;
+	int i = 0;
 
-    while (s[i] != '\0')
-    {
-        i++;
-    }
-    return (i);
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
 }
 
 /**
@@ -43,70 +47,12 @@ int _strlen(char *s)
  */
 void errors(void)
 {
-    printf("Error\n");
-    exit(1);
+	printf("Error\n");
+	exit(98);
 }
 
 /**
- * multiply_numbers - multiplies two positive numbers represented as strings
- * @s1: first number
- * @s2: second number
- * @result: array to store the result
- * @len: length of the result array
- */
-void multiply_numbers(char *s1, char *s2, int *result, int len)
-{
-    int len1, len2, i, carry, digit1, digit2;
-    len1 = _strlen(s1);
-    len2 = _strlen(s2);
-
-    for (i = 0; i < len; i++)
-    {
-        result[i] = 0;
-    }
-    for (len1 = len1 - 1; len1 >= 0; len1--)
-    {
-        digit1 = s1[len1] - '0';
-        carry = 0;
-        for (len2 = len2 - 1; len2 >= 0; len2--)
-        {
-            digit2 = s2[len2] - '0';
-            carry += result[len1 + len2 + 1] + (digit1 * digit2);
-            result[len1 + len2 + 1] = carry % 10;
-            carry /= 10;
-        }
-        if (carry > 0)
-        {
-            result[len1 + len2 + 1] += carry;
-        }
-    }
-}
-
-/**
- * print_result - prints the result array
- * @result: array to print
- * @len: length of the array
- */
-void print_result(int *result, int len)
-{
-    int i, a = 0;
-    for (i = 0; i < len - 1; i++)
-    {
-        if (result[i])
-        {
-            a = 1;
-            _putchar(result[i]);
-        }
-    }
-    if (!a)
-    {
-        _putchar("0");
-    }
-    _putchar("\n");
-}
-
-/**
- * main - entry point of the program
+ * main - multiplies two positive numbers
  * @argc: number of arguments
  * @argv: array of arguments
  *
@@ -114,25 +60,44 @@ void print_result(int *result, int len)
  */
 int main(int argc, char *argv[])
 {
-    char *s1, *s2;
-    int len1, len2, len, *result;
+	char *s1, *s2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
-    if (argc != 3 || !is_digit(argv[1]) || !is_digit(argv[2]))
-    {
-        errors();
-    }
-    s1 = argv[1];
-    s2 = argv[2];
-    len1 = _strlen(s1);
-    len2 = _strlen(s2);
-    len = len1 + len2 + 1;
-    result = (int *)malloc(sizeof(int) * len);
-    if (!result)
-    {
-        return (1);
-    }
-    multiply_numbers(s1, s2, result, len);
-    print_result(result, len);
-    free(result);
-    return (0);
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+		errors();
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
+	if (!result)
+		return (1);
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)
+	{
+		digit1 = s1[len1] - '0';
+		carry = 0;
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+		{
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
+		}
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
+	}
+	for (i = 0; i < len - 1; i++)
+	{
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
+	}
+	if (!a)
+		_putchar('0');
+	_putchar('\n');
+	free(result);
+	return (0);
 }
