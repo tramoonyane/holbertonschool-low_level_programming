d5310b4013be546d838f7d4579a6a6eb7f772123
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-11-11 13:19:13 +0200

    Update 101-mul.c

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -71,7 +71,6 @@ int main(int argc, char *argv[])
 {
 char *s1, *s2;
 int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
-    
 s1 = argv[1], s2 = argv[2];
 if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 exit(98);
@@ -90,7 +89,7 @@ carry = 0;
 for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 {
 digit2 = s2[len2] - '0';
-carry += result[len1 + len2 + 1] + (digit1 * digit2);
+carry += result[len1 + len2 + 1] + (digit1 *digit2);
 result[len1 + len2 + 1] = carry % 10;
 carry /= 10;
 }
