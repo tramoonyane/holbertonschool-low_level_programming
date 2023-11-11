3b5865036662b0eaacc8fdda4376f6bef8c5cd76
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-11-11 21:11:01 +0200

    Update 101-mul.c

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -3,25 +3,6 @@
 #include <stdio.h>
 
 /**
- * is_digit - checks if a string contains a non-digit char
- * @s: string to be evaluated
- *
- * Return: 0 if a non-digit is found, 1 otherwise
- */
-int is_digit(char *s)
-{
-int i = 0;
-
-while (s[i])
-{
-if (s[i] < '0' || s[i] > '9')
-return (0);
-i++;
-}
-return (1);
-}
-
-/**
  * _strlen - returns the length of a string
  * @s: string to evaluate
  *
