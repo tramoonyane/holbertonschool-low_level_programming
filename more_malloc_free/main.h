653ff035123fc15909d020e1634aaab3e89cf183
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-11-12 09:51:27 +0200

    Update main.h

--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -7,6 +7,8 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 int _putchar(char c);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-int is_digit(char *s);
+char *_memset(char *s, char b, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+void mul(char *s1, char *s2);
 
 #endif
