5c3c3dfacdd85a8f17f38f49771633f23368a649
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-11-12 10:14:30 +0200

    Update 101-mul.c

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -46,24 +46,17 @@ void mul(char *s1, char *s2)
 	int i, l1, l2, total_l, f_digit, s_digit, res = 0, tmp;
 	char *ptr;
 	void *temp;
-
 	/* Calculate lengths of input strings */
 	l1 = 0;
 	while (s1[l1] != '\0')
 		l1++;
-
 	l2 = 0;
 	while (s2[l2] != '\0')
 		l2++;
-	/* Store the original length of s2 for later use */
 	tmp = l2;
-	/* Calculate the total length of the result */
 	total_l = l1 + l2;
-	/* Dynamically allocate memory for the result */
 	ptr = _calloc(sizeof(int), total_l);
-	/* store our pointer address to free later */
 	temp = ptr;
-	/* Perform multiplication algorithm */
 	for (l1--; l1 >= 0; l1--)
 	{
 		f_digit = s1[l1] - '0';
@@ -79,19 +72,14 @@ void mul(char *s1, char *s2)
 		if (res)
 			ptr[l1 + l2 + 1] = res % 10;
 	}
-
-	/* Normalize result by removing leading zeros */
 	while (*ptr == 0)
 	{
 		ptr++;
 		total_l--;
 	}
-
-	/* Print the final result */
 	for (i = 0; i < total_l; i++)
 		printf("%i", ptr[i]);
 	printf("\n");
-
 	/* Free dynamically allocated memory */
 	free(temp);
 }
