abd6c40393008a8f0c0166a05ea18bf2a69f90e4
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-11-13 17:47:15 +0200

    Update 4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -14,9 +14,10 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+dog_t *new_dog;
 if (name == NULL || owner == NULL)
 return (NULL);
-dog_t *new_dog = malloc(sizeof(dog_t));
+new_dog = malloc(sizeof(dog_t));
 if (new_dog == NULL)
 return (NULL);
 new_dog->name = strdup(name);
