0a333d0a6047a777914769bcf92ade241bb5eae2
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-11-13 16:48:54 +0200

    Update 2-print_dog.c

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -11,10 +11,10 @@
  */
 void print_dog(struct dog *d)
 {
-    if (d == NULL)
-        return;
+if (d == NULL)
+return;
 
-    printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
-    printf("Age: %.6f\n", d->age);
-    printf("Owner: %s\n", (d->owner != NULL) ? d->owner : "(nil)");
+printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
+printf("Age: %.6f\n", d->age);
+printf("Owner: %s\n", (d->owner != NULL) ? d->owner : "(nil)");
 }
