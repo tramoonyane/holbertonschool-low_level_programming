0ab6781009dd01ab53bbee2c3f6a16bdc60b4111
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-11-13 20:35:54 +0200

    Update 4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "dog.h"
 
 /**
@@ -15,40 +14,27 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *new_dog;
+
 if (name == NULL || owner == NULL)
 return (NULL);
 new_dog = malloc(sizeof(dog_t));
 if (new_dog == NULL)
-return (NULL);
-new_dog->name = strdup(name);
+exit(EXIT_FAILURE);
+new_dog->name = malloc((_strlen(name) + 1) * sizeof(char));
 if (new_dog->name == NULL)
 {
 free(new_dog);
-return (NULL);
+exit(EXIT_FAILURE);
 }
+_strcpy(new_dog->name, name);
 new_dog->age = age;
-new_dog->owner = strdup(owner);
+new_dog->owner = malloc((_strlen(owner) + 1) * sizeof(char));
 if (new_dog->owner == NULL)
 {
 free(new_dog->name);
 free(new_dog);
-return (NULL);
+exit(EXIT_FAILURE);
 }
+_strcpy(new_dog->owner, owner);
 return (new_dog);
-}
-/**
- * free_dog - Frees memory allocated for a dog structure.
- * @d: Pointer to a dog_t structure.
- *
- * Description: This function frees the memory allocated for the
- * name, owner, and the dog structure itself. It should be called
- * when the dog structure is no longer needed to avoid memory leaks.
- */
-void free_dog(dog_t *d)
-{
-if (d == NULL)
-return;
-free(d->name);
-free(d->owner);
-free(d);
 }
