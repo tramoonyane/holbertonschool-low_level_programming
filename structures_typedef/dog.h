72d48836fa8666ab3e89a8b7f0060a4eed053adb
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-11-13 15:26:28 +0200

    Update dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,1 +1,15 @@
-##
+/**
+ * struct dog - Represents information about a dog.
+ * @name: Pointer to a string representing the name of the dog.
+ * @age: The age of the dog as a floating-point number.
+ * @owner: Pointer to a string representing the owner of the dog.
+ *
+ * Description: This struct encapsulates information about a dog,
+ * including its name, age, and owner. It is designed to be used
+ * for creating variables that represent individual dogs.
+ */
+struct dog {
+    char *name;
+    float age;
+    char *owner;
+};
