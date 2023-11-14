0ea9e3942ecaea4f80d8ba5eb7d6a243748fe504
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-11-14 19:35:14 +0200

    Update 2-int_index.c

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -14,15 +14,16 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-    if (array != NULL && cmp != NULL && size > 0) {
-        int i;
+if (array != NULL && cmp != NULL && size > 0)
+{
+int i;
 
-        for (i = 0; i < size; i++) {
-            if (cmp(array[i]) != 0) {
-                return i;
-            }
-        }
-    }
-    
-    return -1; 
+for (i = 0; i < size; i++)
+{
+if (cmp(array[i]) != 0)
+{
+return (i);
 }
+}
+}
+return (-1);}
