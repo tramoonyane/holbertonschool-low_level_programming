1fba481f86f6bcf9e8d0c675eca1c64bf764c780
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-11-15 01:36:15 +0200

    Update main.c

--- variadic_functions/main.c
+++ variadic_functions/main.c
@@ -1,1 +1,14 @@
-#
+/**
+ * main - Entry point of the C program.
+ *
+ * Description: The main function serves as the entry point of the program.
+ *              It calls the asm_main function written in assembly to print
+ *              "Hello, World" using system calls.
+ *
+ * Return: Always returns 0 to indicate successful execution.
+ */
+int main(void)
+{
+asm_main();
+return (0);
+}
