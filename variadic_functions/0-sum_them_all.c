a8bfa31627052c6d926e32906ea03501a548a552
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-11-14 23:15:06 +0200

    Update 0-sum_them_all.c

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -9,12 +9,11 @@
 int sum_them_all(const unsigned int n, ...)
 {
 va_list args;
-if (n == 0)
-return (0);
-
 unsigned int i;
 int sum = 0;
 
+if (n == 0)
+return (0);
 va_start(args, n);
 for (i = 0; i < n; i++)
 {
