cf386eed96660144df908c4af12e539e3f35ffe4
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-11-14 20:22:06 +0200

    Update 3-main.c

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,1 +1,37 @@
-#
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - Entry point
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+int num1, num2, result;
+int (*op_func)(int, int);
+
+if (argc != 4)
+{
+printf("Error\n");
+return (98);
+}
+num1 = atoi(argv[1]);
+num2 = atoi(argv[3]);
+op_func = get_op_func(argv[2]);
+if (op_func == NULL)
+{
+printf("Error\n");
+return (99);
+}
+if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
+{
+printf("Error\n");
+return (100);
+}
+result = op_func(num1, num2);
+printf("%d\n", result);
+return (0);
+}
