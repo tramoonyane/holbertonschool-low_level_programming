a3e701a29e074374847fb4ed7643492615ee6f3f
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-11-14 20:13:33 +0200

    Update 3-op_functions.c

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,1 +1,23 @@
-#
+#include "3-calc.h"
+
+/* Functions */
+
+int op_add(int a, int b) {
+    return a + b;
+}
+
+int op_sub(int a, int b) {
+    return a - b;
+}
+
+int op_mul(int a, int b) {
+    return a * b;
+}
+
+int op_div(int a, int b) {
+    return a / b;
+}
+
+int op_mod(int a, int b) {
+    return a % b;
+}
