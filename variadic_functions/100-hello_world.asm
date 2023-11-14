d24a42533982aaa2786e440dca3f00b5b18a9fe1
tramoonyane <theteleramoonyane5@gmail.com>
2023-11-14 15:08:59 -0800

    added advanced task on C - Variadic functions

---
+++ variadic_functions/100-hello_world.asm
@@ -0,0 +1,20 @@
+	section .data
+	hello db "Hello, World", 0xA ; '0xA' represents a newline character
+	hello_len equ $ - hello
+
+	section .text
+	global _start
+
+_start:
+	;;  File descriptor for standard output (stdout) is 1
+	mov rdi, 1          ; File descriptor: stdout
+	mov rsi, hello  ; Message to be printed
+	mov rdx, hello_len  ; Length of the message
+	mov rax, 1	    ; Syscall number for sys_write
+	syscall	    ; Call kernel
+
+	;;  Exit the program
+	mov rax, 60         ; Syscall number for sys_exit
+	xor rdi, rdi    ; Exit code 0
+	syscall	; Call kernel
+	
