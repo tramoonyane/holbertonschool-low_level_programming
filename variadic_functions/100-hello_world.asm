5615459a455a0649bee47742b5c26e8ed1eb3145
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-11-15 01:25:06 +0200

    Update 100-hello_world.asm

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -1,19 +1,19 @@
-	section .data
-	hello db "Hello, World", 0xA ; '0xA' represents a newline character
-	hello_len equ $ - hello
+section .data
+    hello db "Hello, World", 0xA ; '0xA' represents a newline character
+    hello_len equ $ - hello
 
-	section .text
-	global _start
+section .text
+    global asm_main
 
-_start:
-	;;  File descriptor for standard output (stdout) is 1
-	mov rdi, 1          ; File descriptor: stdout
-	mov rsi, hello  ; Message to be printed
-	mov rdx, hello_len  ; Length of the message
-	mov rax, 1	    ; Syscall number for sys_write
-	syscall	    ; Call kernel
+asm_main:
+    ;; File descriptor for standard output (stdout) is 1
+    mov rdi, 1          ; File descriptor: stdout
+    mov rsi, hello      ; Message to be printed
+    mov rdx, hello_len  ; Length of the message
+    mov rax, 1          ; Syscall number for sys_write
+    syscall             ; Call kernel
 
-	;;  Exit the program
-	mov rax, 60         ; Syscall number for sys_exit
-	xor rdi, rdi    ; Exit code 0
-	syscall	; Call kernel
+    ;; Exit the program
+    mov rax, 60         ; Syscall number for sys_exit
+    xor rdi, rdi        ; Exit code 0
+    syscall             ; Call kernel
