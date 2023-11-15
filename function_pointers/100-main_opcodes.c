9b13da33fe4689ee438b6cac95b9f673d824e861
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-11-15 23:00:43 +0200

    Update 100-main_opcodes.c

--- function_pointers/100-main_opcodes.c
+++ function_pointers/100-main_opcodes.c
@@ -1,6 +1,6 @@
-#include <stdlib.h>
-#include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <udis86.h>
 
 /**
  * main - Prints the opcodes of its own main function
@@ -11,25 +11,26 @@
  */
 int main(int argc, char *argv[])
 {
-int i;
 if (argc != 2)
 {
-write(2, "Error\n", 6);
+fprintf(stderr, "Error\n");
 exit(1);
 }
 int num_bytes = atoi(argv[1]);
 if (num_bytes < 0)
 {
-write(2, "Error\n", 6);
+fprintf(stderr, "Error\n");
 exit(2);
 }
 unsigned char *ptr = (unsigned char *)main;
-for (i = 0; i < num_bytes; i++)
+ud_t ud_obj;
+ud_init(&ud_obj);
+ud_set_input_buffer(&ud_obj, ptr, num_bytes);
+ud_set_mode(&ud_obj, 64);
+ud_set_syntax(&ud_obj, UD_SYN_INTEL);
+while (ud_disassemble(&ud_obj))
 {
-char hex[3];
-sprintf(hex, "%02x", *(ptr + i));
-write(1, hex, 2);
+printf("\t%s\n", ud_insn_hex(&ud_obj));
 }
-write(1, "\n", 1);
 return (0);
 }
