6f2dd1e3a561cbb8779e861f3e6bba0be30c27e4
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-11-28 00:25:47 +0200

    Update 0-print_list.c

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -10,19 +10,17 @@
  */
 size_t print_list(const list_t *h)
 {
-    const list_t *current = h;
-    size_t node_count = 0;
+const list_t *current = h;
+size_t node_count = 0;
 
-    while (current != NULL)
-    {
-        if (current->str == NULL)
-            printf("[0] (nil)\n");
-        else
-            printf("[%u] %s\n", current->len, current->str);
-
-        node_count++;
-        current = current->next;
-    }
-
-    return (node_count);
+while (current != NULL)
+{
+if (current->str == NULL)
+printf("[0] (nil)\n");
+else
+printf("[%u] %s\n", current->len, current->str);
+node_count++;
+current = current->next;
+}
+return (node_count);
 }
