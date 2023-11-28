6ff1e1db9fe1ca672d7435ebdb31524d87bf8758
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-11-28 03:03:17 +0200

    Update 3-add_node_end.c

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -11,8 +11,8 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-list_t *new_node = malloc(sizeof(list_t));
-list_t *temp = *head;
+list_t *new_node;
+list_t *temp;
 if (str == NULL)
 return (NULL);
 new_node = malloc(sizeof(list_t));
