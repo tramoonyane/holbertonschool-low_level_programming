532acf39001df5e03278c404e6a9e953ed6e2122
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-11-28 23:41:19 +0200

    Update 2-add_dnodeint.c

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -10,23 +10,22 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-    dlistint_t *new_node;
+dlistint_t *new_node;
 
-    /* Allocate memory for new node */
-    new_node = malloc(sizeof(dlistint_t));
-    if (new_node == NULL)
-        (return NULL);
+/* Allocate memory for new node */
+new_node = malloc(sizeof(dlistint_t));
+if (new_node == NULL)
+(return NULL);
 
-    /* Initialize the new node */
-    new_node->n = n;
-    new_node->prev = NULL;
-    new_node->next = *head;
+/* Initialize the new node */
+new_node->n = n;
+new_node->prev = NULL;
+new_node->next = *head;
 
-    if (*head != NULL)
-        (*head)->prev = new_node;
+if (*head != NULL)
+(*head)->prev = new_node;
 
-    /* Update head to point to the new node */
-    *head = new_node;
-
-    return (new_node);
+/* Update head to point to the new node */
+*head = new_node;
+return (new_node);
 }
