f19d72258cb63221e2ba70cab413e32b3a6fa15b
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-11-29 20:37:07 +0200

    Update 3-set_bit.c

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
  * set_bit - sets the value of a bit to 1 at a given index
@@ -9,11 +10,12 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
+unsigned long int mask;
 if (index >= sizeof(unsigned long int) * 8 || n == NULL)
 {
 return (-1);
 }
-unsigned long int mask = 1UL << index;
+mask = 1UL << index;
 *n |= mask;
 return (1);
 }
