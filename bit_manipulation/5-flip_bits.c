64bd241581a4822df8283fc55bd56def5343c8a4
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-11-29 19:49:58 +0200

    Update 5-flip_bits.c

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * flip_bits - returns the number of bits to flip to get from n to m
+ * @n: unsigned long int number 1
+ * @m: unsigned long int number 2
+ *
+ * Return: number of bits to flip
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+unsigned long int xor_result = n ^ m;
+unsigned int count = 0;
+
+while (xor_result)
+{
+count += xor_result & 1;
+xor_result >>= 1;
+}
+return (count);
+}
