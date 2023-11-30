1df8de9e8eb8f829c0edf7db57fae0b6a1a63dcd
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-11-30 23:19:51 +0200

    Update 100-first.c

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -1,22 +1,11 @@
 #include "lists.h"
-#include <unistd.h>
+#include <stdio.h>
 
 /**
  * before_main - prints specific lines before main function
  */
 void before_main(void)
 {
-char *line1 = "You're beat! and yet, you must allow,\n";
-char *line2 = "I bore my house upon my back!\n";
-
-while (*line1)
-{
-_putchar(*line1);
-line1++;
-}
-while (*line2)
-{
-_putchar(*line2);
-line2++;
-}
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
 }
