7971f411c3413e1ae82e4738520523802904d2df
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-12-04 17:00:15 +0200

    Update 0-read_textfile.c

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,21 +1,11 @@
 #include "main.h"
 
 /**
- * read_textfile - Reads a text file and prints it 
- * to the POSIX standard output
- * @filename: The name of the file to read
- * @letters: The number of letters it should read and print
+ * read_textfile - Reads a text file and prints it to the POSIX standard output.
+ * @filename: The name of the file to read.
+ * @letters: The number of letters it should read and print.
  *
- * Return: The actual number of letters it could read and print, or 0 on failure
- */
-#include "main.h"
-
-/**
- * read_textfile - Reads a text file and prints it to the POSIX standard output
- * @filename: The name of the file to read
- * @letters: The number of letters it should read and print
- *
- * Return: The actual number of letters it could read and print, or 0 on failure
+ * Return: The actual number of letters it could read and print, or 0 on failure.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
