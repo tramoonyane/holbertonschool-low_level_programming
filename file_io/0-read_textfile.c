522d628da66da838d26cbbbe6a5c322097339532
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-12-04 17:30:41 +0200

    Update 0-read_textfile.c

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -7,9 +7,11 @@
  * @letters: number of letters to read and print
  * Return: number of letters read and printed, 0 on failure
  */
+#define BUFFER_SIZE 1024
+
 ssize_t read_textfile(const char *filename, size_t letters) {
-    ssize_t file_descriptor, bytes_read, bytes_written;
-    char *buffer;
+    ssize_t file_descriptor, bytes_read, bytes_written, total_bytes = 0;
+    char buffer[BUFFER_SIZE];
 
     if (filename == NULL)
         return 0;
@@ -18,28 +20,18 @@ ssize_t read_textfile(const char *filename, size_t letters) {
     if (file_descriptor == -1)
         return 0;
 
-    buffer = malloc(sizeof(char) * letters);
-    if (buffer == NULL) {
-        close(file_descriptor);
-        return 0;
+    while ((bytes_read = read(file_descriptor, buffer, BUFFER_SIZE)) > 0 && total_bytes < letters) {
+        bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+        if (bytes_written == -1 || bytes_written != bytes_read) {
+            close(file_descriptor);
+            return total_bytes;
+        }
+        total_bytes += bytes_written;
+        if (total_bytes >= letters)
+            break;
     }
 
-    bytes_read = read(file_descriptor, buffer, letters);
-    if (bytes_read == -1) {
-        free(buffer);
-        close(file_descriptor);
-        return 0;
-    }
-
-    bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-    if (bytes_written == -1 || bytes_written != bytes_read) {
-        free(buffer);
-        close(file_descriptor);
-        return 0;
-    }
-
-    free(buffer);
     close(file_descriptor);
 
-    return bytes_written;
+    return total_bytes;
 }
