191b165e0c76ddc89eeb6fdd620dd4fdd9bf4a17
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-12-04 16:13:13 +0200

    Update 0-read_textfile.c

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * read_textfile - Reads a text file and prints it to the POSIX standard output
+ * @filename: The name of the file to read
+ * @letters: The number of letters it should read and print
+ *
+ * Return: The actual number of letters it could read and print, or 0 on failure
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+    ssize_t file_descriptor, bytes_read, bytes_written;
+    char *buffer;
+
+    if (filename == NULL)
+        return (0);
+
+    file_descriptor = open(filename, O_RDONLY);
+    if (file_descriptor == -1)
+        return (0);
+
+    buffer = malloc(sizeof(char) * letters);
+    if (buffer == NULL)
+    {
+        close(file_descriptor);
+        return (0);
+    }
+
+    bytes_read = read(file_descriptor, buffer, letters);
+    if (bytes_read == -1)
+    {
+        free(buffer);
+        close(file_descriptor);
+        return (0);
+    }
+
+    bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+    if (bytes_written == -1 || (size_t)bytes_written != (size_t)bytes_read)
+    {
+        free(buffer);
+        close(file_descriptor);
+        return (0);
+    }
+
+    free(buffer);
+    close(file_descriptor);
+    return (bytes_read);
+}
