ef5d2fa1aa090a086843d524e9779a5a0d40b8d8
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-12-04 16:49:45 +0200

    Update 0-read_textfile.c

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * read_textfile - Reads a text file and prints it to the POSIX standard output
+ * read_textfile - Reads a text file and prints it 
+ * to the POSIX standard output
  * @filename: The name of the file to read
  * @letters: The number of letters it should read and print
  *
