20a057d97939767c4464369ea16dac89325e6f5b
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-12-05 23:15:48 +0200

    Update 4-hash_table_get.c

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -9,21 +9,18 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-    unsigned long int index;
-    hash_node_t *tmp;
+unsigned long int index;
+hash_node_t *tmp;
 
-    if (ht == NULL || key == NULL || *key == '\0')
-        return (NULL);
-
-    index = key_index((unsigned char *)key, ht->size);
-    tmp = ht->array[index];
-
-    while (tmp != NULL)
-    {
-        if (strcmp(tmp->key, key) == 0)
-            return (tmp->value);
-        tmp = tmp->next;
-    }
-
-    return (NULL);
+if (ht == NULL || key == NULL || *key == '\0')
+return (NULL);
+index = key_index((unsigned char *)key, ht->size);
+tmp = ht->array[index];
+while (tmp != NULL)
+{
+if (strcmp(tmp->key, key) == 0)
+return (tmp->value);
+tmp = tmp->next;
+}
+return (NULL);
 }
