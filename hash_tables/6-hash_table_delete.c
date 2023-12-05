ca10b7c7fc054b98ef0c8642a8011b70199858ec
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-12-05 23:22:01 +0200

    Update 6-hash_table_delete.c

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -6,24 +6,23 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-    unsigned long int i;
-    hash_node_t *node, *tmp;
+unsigned long int i;
+hash_node_t *node, *tmp;
 
-    if (ht == NULL)
-        return;
-
-    for (i = 0; i < ht->size; i++)
-    {
-        node = ht->array[i];
-        while (node != NULL)
-        {
-            tmp = node->next;
-            free(node->key);
-            free(node->value);
-            free(node);
-            node = tmp;
-        }
-    }
-    free(ht->array);
-    free(ht);
+if (ht == NULL)
+return;
+for (i = 0; i < ht->size; i++)
+{
+node = ht->array[i];
+while (node != NULL)
+{
+tmp = node->next;
+free(node->key);
+free(node->value);
+free(node);
+node = tmp;
+}
+}
+free(ht->array);
+free(ht);
 }
