6157693e7b3ecf19c715a1e16d08b807aee7b8b7
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-12-05 23:05:51 +0200

    Update 3-hash_table_set.c

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -9,51 +9,42 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    unsigned long int index;
-    hash_node_t *new_node, *tmp;
+unsigned long int index;
+hash_node_t *new_node, *tmp;
 
-    if (ht == NULL || key == NULL || *key == '\0')
-        return (0);
-
-    index = key_index((unsigned char *)key, ht->size);
-
-    /* Check if key already exists; if yes, update the value */
-    tmp = ht->array[index];
-    while (tmp != NULL)
-    {
-        if (strcmp(tmp->key, key) == 0)
-        {
-            free(tmp->value);
-            tmp->value = strdup(value);
-            if (tmp->value == NULL)
-                return (0);
-            return (1);
-        }
-        tmp = tmp->next;
-    }
-
-    /* If key doesn't exist, create a new node */
-    new_node = malloc(sizeof(hash_node_t));
-    if (new_node == NULL)
-        return (0);
-
-    new_node->key = strdup(key);
-    if (new_node->key == NULL)
-    {
-        free(new_node);
-        return (0);
-    }
-
-    new_node->value = strdup(value);
-    if (new_node->value == NULL)
-    {
-        free(new_node->key);
-        free(new_node);
-        return (0);
-    }
-
-    new_node->next = ht->array[index];
-    ht->array[index] = new_node;
-
-    return (1);
+if (ht == NULL || key == NULL || *key == '\0')
+return (0);
+index = key_index((unsigned char *)key, ht->size);
+tmp = ht->array[index];
+while (tmp != NULL)
+{
+if (strcmp(tmp->key, key) == 0)
+{
+free(tmp->value);
+tmp->value = strdup(value);
+if (tmp->value == NULL)
+return (0);
+return (1);
+}
+tmp = tmp->next;
+}
+new_node = malloc(sizeof(hash_node_t));
+if (new_node == NULL)
+return (0);
+new_node->key = strdup(key);
+if (new_node->key == NULL)
+{
+free(new_node);
+return (0);
+}
+new_node->value = strdup(value);
+if (new_node->value == NULL)
+{
+free(new_node->key);
+free(new_node);
+return (0);
+}
+new_node->next = ht->array[index];
+ht->array[index] = new_node;
+return (1);
 }
