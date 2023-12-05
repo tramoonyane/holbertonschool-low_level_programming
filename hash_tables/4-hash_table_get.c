dd193c5ebef2631590a91f01005daf2363ff42cb
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-12-05 21:50:26 +0200

    Update 4-hash_table_get.c

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - Retrieves a value associated with a key
+ * @ht: The hash table to look into
+ * @key: The key to search for
+ *
+ * Return: The value associated with the key, or NULL if key couldn't be found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+    unsigned long int index;
+    hash_node_t *tmp;
+
+    if (ht == NULL || key == NULL || *key == '\0')
+        return (NULL);
+
+    index = key_index((unsigned char *)key, ht->size);
+    tmp = ht->array[index];
+
+    while (tmp != NULL)
+    {
+        if (strcmp(tmp->key, key) == 0)
+            return (tmp->value);
+        tmp = tmp->next;
+    }
+
+    return (NULL);
+}
