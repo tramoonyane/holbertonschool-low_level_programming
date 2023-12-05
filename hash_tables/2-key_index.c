78bcd3f9926be75451e00113d95abd13ab24ad0a
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-12-05 22:34:14 +0200

    Update 2-key_index.c

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -9,8 +9,8 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-    unsigned long int hash_val;
+unsigned long int hash_val;
+hash_val = hash_djb2(key);
 
-    hash_val = hash_djb2(key);
-    return (hash_val % size);
+return (hash_val % size);
 }
