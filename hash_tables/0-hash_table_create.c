412aaea25eabc0ed9cf753e4d91848297b619858
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-12-05 22:25:20 +0200

    Update 0-hash_table_create.c

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -9,24 +9,20 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *new_table = NULL;
-    unsigned long int i;
+hash_table_t *new_table = NULL;
+unsigned long int i;
 
-    new_table = malloc(sizeof(hash_table_t));
-    if (new_table == NULL)
-        return (NULL);
-
-    new_table->size = size;
-
-    new_table->array = malloc(sizeof(hash_node_t *) * size);
-    if (new_table->array == NULL)
-    {
-        free(new_table);
-        return (NULL);
-    }
-
-    for (i = 0; i < size; i++)
-        new_table->array[i] = NULL;
-
-    return (new_table);
+new_table = malloc(sizeof(hash_table_t));
+if (new_table == NULL)
+return (NULL);
+new_table->size = size;
+new_table->array = malloc(sizeof(hash_node_t *) * size);
+if (new_table->array == NULL)
+{
+free(new_table);
+return (NULL);
+}
+for (i = 0; i < size; i++)
+new_table->array[i] = NULL;
+return (new_table);
 }
