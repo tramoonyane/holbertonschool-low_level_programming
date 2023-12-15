150f6f505d3f5c5a1a6926284349fb3ecc42f406
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-12-15 17:42:17 +0200

    Update Simple_Shell.h

--- preparation_for_C_simple_shell/Simple_Shell.h
+++ preparation_for_C_simple_shell/Simple_Shell.h
@@ -1,15 +1,11 @@
-#ifndef SHELL_H
-#define SHELL_H
+#ifndef SIMPLE_SHELL_H
+#define SIMPLE_SHELL_H
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 
-#define BUFFER_SIZE 1024
+void display_prompt();
+char* read_command();
 
-void prompt();
-
-#endif /* SHELL_H */
+#endif /* SIMPLE_SHELL_H */
