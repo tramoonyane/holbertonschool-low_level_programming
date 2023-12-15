c64ce8874acbec569e9b17137bee53cf74f14040
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-12-15 17:44:27 +0200

    Update simple_shell.c

--- preparation_for_C_simple_shell/simple_shell.c
+++ preparation_for_C_simple_shell/simple_shell.c
@@ -1,44 +1,48 @@
-#include "Simple_Shell.h"
+#include "simple_shell.h"
 
-void prompt() {
-    printf("($)");
-    fflush(stdout); /* Flush the output buffer */
+#define BUFFER_SIZE 1024
+#define PROMPT "$ "
+
+void display_prompt() {
+    printf("%s", PROMPT);
 }
 
-int main(void) {
-    char buffer[BUFFER_SIZE];
-    char *argv[2];
-    pid_t pid;
+char* read_command() {
+    char input[BUFFER_SIZE];
 
-    char *program_name = "./hsh"; /* Change this accordingly if your executable has a different name */
+    if (fgets(input, BUFFER_SIZE, stdin) == NULL) {
+        perror("fgets error");
+        exit(EXIT_FAILURE);
+    }
 
+    input[strcspn(input, "\n")] = '\0'; /* Remove trailing newline */
+
+    char *command = (char *)malloc(strlen(input) + 1);
+    if (command == NULL) {
+        perror("malloc error");
+        exit(EXIT_FAILURE);
+    }
+
+    strcpy(command, input);
+    return command;
+}
+
+int main() {
     while (1) {
-        prompt(); /* Print the initial prompt before waiting for input */
+        display_prompt();
 
-        if (fgets(buffer, BUFFER_SIZE, stdin) == NULL) {
-            printf("\n");
-            break;  /* End of file (Ctrl+D) */
+        char *command = read_command();
+        printf("Command entered: %s\n", command);
+
+        /* Exit the loop if the user inputs 'exit' */
+        if (strcmp(command, "exit") == 0) {
+            printf("Exiting...\n");
+            free(command);
+            break;
         }
 
-        buffer[strcspn(buffer, "\n")] = '\0';
-
-        pid = fork();
-
-        if (pid == -1) {
-            perror(program_name);
-            exit(EXIT_FAILURE);
-        } else if (pid == 0) {
-            argv[0] = buffer;
-            argv[1] = NULL;
-
-            if (execve(argv[0], argv, NULL) == -1) {
-                perror(program_name);
-                exit(EXIT_FAILURE);
-            }
-        } else {
-            wait(NULL);
-        }
-
+        /* Free memory allocated for command */
+        free(command);
     }
 
     return EXIT_SUCCESS;
