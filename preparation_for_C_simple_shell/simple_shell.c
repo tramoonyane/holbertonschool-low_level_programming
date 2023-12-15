d9d991ab32b48fb4cd15a4b20aa967e780f0b80e
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-12-15 11:35:58 +0200

    Update simple_shell.c

--- preparation_for_C_simple_shell/simple_shell.c
+++ preparation_for_C_simple_shell/simple_shell.c
@@ -1,35 +1,25 @@
 #include "Simple_Shell.h"
-/**
- * tokenize_input - Tokenize user input
- * @input: User input string
- *
- * Return: Array of pointers to tokens (arguments)
- */
-char **tokenize_input(char *input)
-{
+
+char **tokenize_input(char *input) {
     int bufsize = MAX_ARGUMENTS;
     int position = 0;
     char **tokens = malloc(bufsize * sizeof(char *));
     char *token;
 
-    if (!tokens)
-    {
+    if (!tokens) {
         perror("malloc");
         exit(EXIT_FAILURE);
     }
 
     token = strtok(input, TOKEN_DELIM);
-    while (token != NULL)
-    {
+    while (token != NULL) {
         tokens[position] = token;
         position++;
 
-        if (position >= bufsize)
-        {
+        if (position >= bufsize) {
             bufsize += MAX_ARGUMENTS;
             tokens = realloc(tokens, bufsize * sizeof(char *));
-            if (!tokens)
-            {
+            if (!tokens) {
                 perror("realloc");
                 exit(EXIT_FAILURE);
             }
@@ -38,15 +28,9 @@ char **tokenize_input(char *input)
         token = strtok(NULL, TOKEN_DELIM);
     }
     tokens[position] = NULL;
-    return (tokens);
+    return tokens;
 }
-/**
- * execute_command - Execute a command with given arguments
- * @arguments: Array of pointers to tokens (arguments)
- *
- * Description: This function takes the parsed input command
- *              and executes it with the given arguments.
- */
+
 void execute_command(char **arguments) {
     pid_t pid;
     int status;
@@ -66,20 +50,13 @@ void execute_command(char **arguments) {
         }
     } else {
         waitpid(pid, &status, 0);
-        if (WIFEXITED(status)) {
-            status = WEXITSTATUS(status);
-            exit(status);
-        } else {
+        if (!WIFEXITED(status)) {
             exit(EXIT_FAILURE);
         }
+        exit(WEXITSTATUS(status));
     }
 }
 
-/**
- * print_prompt - Display shell prompt
- *
- * Description: This function prints the shell prompt.
- */
 void print_prompt(int status) {
     if (status == 0) {
         printf("($)");
