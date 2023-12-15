116281501f279a80f791766ef6b2821b5dee7042
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-12-15 20:41:12 +0200

    Update simple_shell_path.c

---
+++ preparation_for_C_simple_shell/simple_shell_path.c
@@ -0,0 +1,115 @@
+#include "simple_shell.h"
+
+void display_prompt() {
+    write(STDOUT_FILENO, PROMPT, strlen(PROMPT));
+}
+
+char* read_command() {
+    char input[BUFFER_SIZE];
+
+    if (fgets(input, BUFFER_SIZE, stdin) == NULL) {
+        if (feof(stdin)) {
+            write(STDOUT_FILENO, "\n", 1);
+            exit(EXIT_SUCCESS);
+        } else {
+            perror("fgets error");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    input[strcspn(input, "\n")] = '\0';
+
+    char *command = (char *)malloc(strlen(input) + 1);
+    if (command == NULL) {
+        perror("malloc error");
+        exit(EXIT_FAILURE);
+    }
+
+    strcpy(command, input);
+    return command;
+}
+
+char** parse_arguments(const char *command) {
+    char **args = (char **)malloc(BUFFER_SIZE * sizeof(char *));
+    if (args == NULL) {
+        perror("malloc error");
+        exit(EXIT_FAILURE);
+    }
+
+    char *token = strtok((char *)command, " ");
+    int i = 0;
+    while (token != NULL) {
+        args[i] = token;
+        token = strtok(NULL, " ");
+        i++;
+    }
+    args[i] = NULL;
+
+    return args;
+}
+
+int main(int argc, char *argv[]) {
+    char *command;
+
+    do {
+        display_prompt();
+
+        command = read_command();
+
+        if (feof(stdin)) {
+            write(STDOUT_FILENO, "\n", 1);
+            free(command);
+            exit(EXIT_SUCCESS);
+        }
+
+        char **args = parse_arguments(command);
+
+        pid_t pid;
+        if (access(args[0], X_OK) != -1) {
+            pid = fork();
+        } else {
+            char *path;
+            char *env_path = getenv("PATH");
+            char *token = strtok(env_path, ":");
+            while (token != NULL) {
+                path = (char *)malloc(strlen(token) + strlen(args[0]) + 2);
+                strcpy(path, token);
+                strcat(path, "/");
+                strcat(path, args[0]);
+                if (access(path, X_OK) != -1) {
+                    args[0] = path;
+                    pid = fork();
+                    break;
+                }
+                free(path);
+                token = strtok(NULL, ":");
+            }
+            if (token == NULL) {
+                fprintf(stderr, "%s: command not found\n", args[0]);
+                free(command);
+                free(args);
+                continue;
+            }
+        }
+
+        if (pid == -1) {
+            perror("fork error");
+            exit(EXIT_FAILURE);
+        } else if (pid == 0) {
+            if (execv(args[0], args) == -1) {
+                perror("execv error");
+                exit(EXIT_FAILURE);
+            }
+        } else {
+            int status;
+            waitpid(pid, &status, 0);
+        }
+
+        free(args);
+        free(command);
+    } while (strcmp(command, "exit") != 0);
+
+    write(STDOUT_FILENO, "Exiting...\n", strlen("Exiting...\n"));
+
+    return EXIT_SUCCESS;
+}
