31eb444ccc52dfa1aca5b057101fe2c7f0d35a4d
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-12-15 12:36:46 +0200

    Update main.c

--- preparation_for_C_simple_shell/main.c
+++ preparation_for_C_simple_shell/main.c
@@ -6,37 +6,36 @@ int main(void) {
     ssize_t characters_read;
     char **arguments;
     int status = 0;
-    
-           while (1) {
-        
-            print_prompt(status);
-        
+
+    while (1) {
+        print_prompt(status);
+
         characters_read = getline(&buffer, &bufsize, stdin);
         if (characters_read == -1) {
             free(buffer);
+            continue;
         }
 
         if (buffer[characters_read - 1] == '\n') {
             buffer[characters_read - 1] = '\0';
         }
 
-        if (strcmp(buffer, "") == 0) {
-            free(buffer);
-        }
-
         arguments = tokenize_input(buffer);
         if (arguments != NULL) {
             if (strcmp(arguments[0], "exit") == 0) {
-                free(arguments);
                 free(buffer);
+                free(arguments);
                 exit(EXIT_SUCCESS);
-                }
             }
             execute_command(arguments);
             free(arguments);
             status = 0;
-        } 
+        } else {
+            status = 1;
+        }
 
-    free(buffer);
+        free(buffer); // Free allocated memory after command execution
+    }
+
     return 0;
 }
