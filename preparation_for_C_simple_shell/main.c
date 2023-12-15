45db841e51e06ac66f517340f16b650be90bc398
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-12-15 11:34:35 +0200

    Update main.c

--- preparation_for_C_simple_shell/main.c
+++ preparation_for_C_simple_shell/main.c
@@ -7,33 +7,48 @@ int main(void) {
     char **arguments;
     int status = 0;
 
-    while (1) {
-        print_prompt(status);
-        characters_read = getline(&buffer, &bufsize, stdin);
-        if (characters_read == -1) {
-            printf("\n");
-            free(buffer);
-            exit(EXIT_SUCCESS);
-        }
-        if (buffer[characters_read - 1] == '\n') {
-            buffer[characters_read - 1] = '\0';
-        }
-
-        arguments = tokenize_input(buffer);
-        if (arguments != NULL) {
-            if (strcmp(arguments[0], "exit") == 0) {
-                free(arguments);
+    if (isatty(STDIN_FILENO)) {
+        while (1) {
+            print_prompt(status);
+            characters_read = getline(&buffer, &bufsize, stdin);
+            if (characters_read == -1) {
+                printf("\n");
                 free(buffer);
                 exit(EXIT_SUCCESS);
             }
-            execute_command(arguments);
-            free(arguments);
-            status = 0;
-        } else {
-            status = 1;
+            if (buffer[characters_read - 1] == '\n') {
+                buffer[characters_read - 1] = '\0';
+            }
+
+            arguments = tokenize_input(buffer);
+            if (arguments != NULL) {
+                if (strcmp(arguments[0], "exit") == 0) {
+                    free(arguments);
+                    free(buffer);
+                    exit(EXIT_SUCCESS);
+                }
+                execute_command(arguments);
+                free(arguments);
+                status = 0;
+            } else {
+                status = 1;
+            }
         }
+    } else {
+        characters_read = getline(&buffer, &bufsize, stdin);
+        if (characters_read != -1) {
+            if (buffer[characters_read - 1] == '\n') {
+                buffer[characters_read - 1] = '\0';
+            }
+
+            arguments = tokenize_input(buffer);
+            if (arguments != NULL) {
+                execute_command(arguments);
+                free(arguments);
+            }
+        }
+        free(buffer);
     }
 
-    free(buffer);
     return 0;
 }
