326cc673886cb5f0612db6e5b109495bf7c418a6
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-12-15 15:52:25 +0200

    Update Simple_Shell.h

--- preparation_for_C_simple_shell/Simple_Shell.h
+++ preparation_for_C_simple_shell/Simple_Shell.h
@@ -1,21 +1,15 @@
-#ifndef SIMPLE_SHELL_H
-#define SIMPLE_SHELL_H
+#ifndef SHELL_H
+#define SHELL_H
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <string.h>
 
 #define BUFFER_SIZE 1024
-#define TOKEN_DELIM " \t\n"
-#define MAX_ARGUMENTS 64
 
-/* Function prototypes */
-char **tokenize_input(char *input);
-void execute_command(char **arguments);
-void print_prompt(void);
-void print_environment();
+void prompt();
 
-#endif /* SIMPLE_SHELL_H */
+#endif /* SHELL_H */
