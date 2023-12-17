4dc3ffdf4bf22e0c77765b2eea64c781cdb1b4f0
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-12-17 07:17:44 +0200

    Update simple_shell_path.c

--- preparation_for_C_simple_shell/simple_shell_path.c
+++ preparation_for_C_simple_shell/simple_shell_path.c
@@ -1,9 +1,20 @@
 #include "Simple_Shell.h"
 
+/**
+ * display_prompt - Displays the shell prompt.
+ */
 void display_prompt() {
     write(STDOUT_FILENO, PROMPT, strlen(PROMPT));
 }
 
+/**
+ * parse_arguments - Parses the command string into arguments.
+ *
+ * @command: The input command to be parsed.
+ *
+ * Return: Returns an array of strings (arguments) parsed from the command.
+ *         Returns NULL on failure or if no arguments are found.
+ */
 char** parse_arguments(const char *command) {
     char *token;
     int i;
@@ -25,6 +36,11 @@ char** parse_arguments(const char *command) {
     return args;
 }
 
+/**
+ * read_command - Reads a command from standard input.
+ *
+ * Return: Returns the input command as a dynamically allocated string.
+ */
 char* read_command() {
     char *command;
     char input[BUFFER_SIZE];
@@ -51,6 +67,14 @@ char* read_command() {
     return command;
 }
 
+/**
+ * main - Main function of the shell.
+ *
+ * @argc: The number of arguments passed to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Returns EXIT_SUCCESS upon successful execution.
+ */
 int main(int argc __attribute__((unused)), char *argv[] __attribute__((unused))) {
     char *command;
     char **args;
