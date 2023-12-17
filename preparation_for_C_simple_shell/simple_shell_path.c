603098f436843616212d8c00efdfe32691b922cb
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-12-17 03:14:16 +0200

    Update simple_shell_path.c

--- preparation_for_C_simple_shell/simple_shell_path.c
+++ preparation_for_C_simple_shell/simple_shell_path.c
@@ -101,12 +101,12 @@ int main(int argc __attribute__((unused)), char *argv[] __attribute__((unused)))
                     args[0] = path;
                     pid = fork();
                     break;
+                    fprintf(stderr, "%s: command not found\n", args[0]);
                 }
                 free(path);
                 token = strtok(NULL, ":");
             }
             if (token == NULL) {
-                fprintf(stderr, "%s: command not found\n", args[0]);
                 free(command);
                 free(args);
                 continue;
