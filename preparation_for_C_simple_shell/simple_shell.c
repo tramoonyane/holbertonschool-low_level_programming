15789e755e2c777c48ee1b4289b90a1f59f4973e
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-12-18 00:56:51 +0200

    Update simple_shell.c

--- preparation_for_C_simple_shell/simple_shell.c
+++ preparation_for_C_simple_shell/simple_shell.c
@@ -1,1 +1,77 @@
+/* simple_shell.c */
+
+#include "Simple_Shell.h"
+
+/**
+ * read_command - Reads a command from standard input.
+ *
+ * Return: Returns the input command as a dynamically allocated string.
+ */
+char* read_command()
+{
+    char input[BUFFER_SIZE];
+    printf("%s", PROMPT);
+
+    if (fgets(input, BUFFER_SIZE, stdin) == NULL) {
+        if (feof(stdin)) {
+            write(STDOUT_FILENO, "\n", 1);
+            exit(EXIT_SUCCESS);
+        } else {
+            perror("fgets error");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    input[strcspn(input, "\n")] = '\0';
+
+    char* command = strdup(input);
+    if (command == NULL) {
+        perror("strdup error");
+        exit(EXIT_FAILURE);
+    }
+
+    return command;
+}
+
+/**
+ * main - Main function of the shell.
+ *
+ * Return: Returns EXIT_SUCCESS upon successful execution.
+ */
+int main()
+{
+    char *command;
+    pid_t pid;
+    int status;
+
+    do {
+        command = read_command();
+
+        if (feof(stdin)) {
+            free(command);
+            write(STDOUT_FILENO, "\n", 1);
+            exit(EXIT_SUCCESS);
+        }
+
+        pid = fork();
+
+        if (pid == -1) {
+            perror("fork error");
+            exit(EXIT_FAILURE);
+        } else if (pid == 0) {
+            /* Child process */
+            if (execlp(command, command, NULL) == -1) {
+                fprintf(stderr, "%s: command not found\n", command);
+                exit(EXIT_FAILURE);
+            }
+        } else {
+            /* Parent process */
+            waitpid(pid, &status, 0);
+        }
+
+        free(command);
+    } while (1);
+
+    return EXIT_SUCCESS;
+}
 
