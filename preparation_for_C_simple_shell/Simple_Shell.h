0f1c5c92053e9aed52253e416df6995ddf2aeb92
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-12-18 00:31:38 +0200

    Update Simple_Shell.h

--- preparation_for_C_simple_shell/Simple_Shell.h
+++ preparation_for_C_simple_shell/Simple_Shell.h
@@ -3,16 +3,19 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
 #include <sys/wait.h>
 
 #define BUFFER_SIZE 1024
 #define PROMPT "$ "
+#define EXIT_SUCCESS 0
+#define EXIT_FAILURE 1
 
 void display_prompt(void);
 char *read_command(void);
-char **tokenize_command(char *command);
-void execute_command(const char *program_name, char **args);
+char **parse_arguments(const char *command);
+void execute_command(char **args);
 
 #endif /* SIMPLE_SHELL_H */
