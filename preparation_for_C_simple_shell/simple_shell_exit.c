bbcd2099e8a56845e2d246ce67d1fdb066018aef
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-12-18 15:05:28 +0200

    Update simple_shell_exit.c

--- preparation_for_C_simple_shell/simple_shell_exit.c
+++ preparation_for_C_simple_shell/simple_shell_exit.c
@@ -10,7 +10,19 @@ void exit_shell() {
 }
 
 /**
- * handle_builtin_commands - Handles built-in commands like 'exit'.
+ * print_environment - Prints the current environment variables.
+ */
+void print_environment() {
+    extern char **environ;
+    int i = 0;
+    while (environ[i] != NULL) {
+        printf("%s\n", environ[i]);
+        i++;
+    }
+}
+
+/**
+ * handle_builtin_commands - Handles built-in commands like 'exit' and 'env'.
  *
  * @command: The command to execute.
  *
@@ -20,6 +32,9 @@ int handle_builtin_commands(char *command) {
     if (strcmp(command, "exit") == 0) {
         exit_shell(); /* Call the exit_shell function */
         return 1;     /* Return 1 to indicate the command was handled */
+    } else if (strcmp(command, "env") == 0) {
+        print_environment(); /* Print the environment variables */
+        return 1;            /* Return 1 to indicate the command was handled */
     }
     return 0; /* Return 0 for other commands */
 }
