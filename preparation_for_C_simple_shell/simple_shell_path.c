a8f24b746673e85e1c080e6924c3debd260baf78
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-12-18 02:41:56 +0200

    Update simple_shell_path.c

--- preparation_for_C_simple_shell/simple_shell_path.c
+++ preparation_for_C_simple_shell/simple_shell_path.c
@@ -1,65 +1,62 @@
-/* simple_shell_path.c */
-
 #include "Simple_Shell.h"
 
 /**
- * display_prompt - Displays the shell prompt.
- */
-void display_prompt(void)
-{
-    write(STDOUT_FILENO, PROMPT, strlen(PROMPT));
-}
-
-/**
- * parse_arguments - Parses the command string into arguments.
+ * execute_command - Executes the command with arguments.
  *
- * @command: The input command to be parsed.
+ * @command: The command to execute.
  *
- * Return: Returns an array of strings (arguments) parsed from the command.
- *         Returns NULL on failure or if no arguments are found.
+ * Return: Returns EXIT_SUCCESS upon successful execution.
  */
-char **parse_arguments(const char *command)
+int execute_command(char *command)
 {
-    int i;
+    char **args = NULL;
     char *token;
-    char *input_command;
-    char *program_name;
-    char **args = (char **)malloc(BUFFER_SIZE * sizeof(char *));
-    if (args == NULL)
-    {
-        perror("malloc error");
-        exit(EXIT_FAILURE);
+    int status;
+
+    /* Check for empty command */
+    if (command == NULL || *command == '\0')
+        return EXIT_SUCCESS;
+
+    /* Check if command exists in PATH */
+    if (access(command, X_OK) == 0) {
+        /* Execute command directly if it exists */
+        if (execve(command, &command, NULL) == -1) {
+            perror("execve error");
+            exit(EXIT_FAILURE);
+        }
     }
 
-    i = 0;
-    input_command = strdup(command); /* Create a copy of the input command */
-
-    token = strtok(input_command, " ");
-    while (token != NULL)
-    {
-        args[i] = strdup(token); /* Allocate memory for each argument */
-        if (args[i] == NULL)
-        {
+    /* Attempt to execute the command from PATH */
+    char *path = getenv("PATH");
+    token = strtok(path, ":");
+    while (token != NULL) {
+        char *full_path = malloc(strlen(token) + strlen(command) + 2);
+        if (full_path == NULL) {
             perror("malloc error");
             exit(EXIT_FAILURE);
         }
-        token = strtok(NULL, " ");
-        i++;
+        sprintf(full_path, "%s/%s", token, command);
+
+        if (access(full_path, X_OK) == 0) {
+            args = malloc(2 * sizeof(char *));
+            if (args == NULL) {
+                perror("malloc error");
+                exit(EXIT_FAILURE);
+            }
+            args[0] = full_path;
+            args[1] = NULL;
+
+            if (execve(args[0], args, NULL) == -1) {
+                perror("execve error");
+                exit(EXIT_FAILURE);
+            }
+        }
+        free(full_path);
+        token = strtok(NULL, ":");
     }
 
-    program_name = strrchr(args[0], '/');
-    if (program_name != NULL)
-    {
-        /* If '/' is found, the program name starts after the '/' character */
-        program_name++; /* Move the pointer to the program name */
-        args[0] = program_name; /* Update the program name in the arguments array */
-    }
-
-    args[i] = NULL; /* NULL-terminate the arguments array */
-
-    free(input_command); /* Free the copy of the input command */
-
-    return args;
+    fprintf(stderr, "%s: command not found\n", command);
+    return EXIT_FAILURE;
 }
 
 /**
@@ -67,219 +64,52 @@ char **parse_arguments(const char *command)
  *
  * Return: Returns the input command as a dynamically allocated string.
  */
-char *read_command(void)
+char *read_command()
 {
-    char *command = NULL;
-    size_t bufsize = 0;
+    char input[BUFFER_SIZE];
 
-    if (getline(&command, &bufsize, stdin) == -1)
-    {
-        if (feof(stdin))
-        {
+    printf("%s", PROMPT);
+
+    if (fgets(input, BUFFER_SIZE, stdin) == NULL) {
+        if (feof(stdin)) {
             write(STDOUT_FILENO, "\n", 1);
             exit(EXIT_SUCCESS);
-        }
-        else
-        {
-            perror("getline error");
+        } else {
+            perror("fgets error");
             exit(EXIT_FAILURE);
         }
     }
 
-    if (feof(stdin))
-    {
-        write(STDOUT_FILENO, "\n", 1);
-        free(command);
-        exit(EXIT_SUCCESS);
-    }
+    input[strcspn(input, "\n")] = '\0';
 
-    /* Remove newline character from the command */
-    command[strcspn(command, "\n")] = '\0';
-
-    return command;
-}
-
-/**
- * check_command_exists - Checks if a command exists in the PATH.
- *
- * @command: The command to check for existence.
- *
- * Return: Returns 1 if the command exists in the PATH, 0 otherwise.
- */
-int check_command_exists(const char *command)
-{
-    char *path = getenv("PATH");
-    char *path_copy;
-    char *token;
-    if (path == NULL)
-    {
-        fprintf(stderr, "Unable to get PATH\n");
-        exit(EXIT_FAILURE);
-    }
-
-    path_copy = strdup(path);
-    token = strtok(path_copy, ":");
-
-    while (token != NULL)
-    {
-        char *full_path = (char *)malloc(strlen(token) + strlen(command) + 2);
-        if (full_path == NULL)
-        {
-            perror("malloc error");
-            exit(EXIT_FAILURE);
-        }
-        sprintf(full_path, "%s/%s", token, command);
-
-        if (access(full_path, X_OK) == 0)
-        {
-            free(path_copy);
-            free(full_path);
-            return 1; /* Command exists in the PATH */
-        }
-
-        free(full_path);
-        token = strtok(NULL, ":");
-    }
-
-    free(path_copy);
-    return 0; /* Command not found in the PATH */
-}
-
-/**
- * execute_command - Executes the command if it exists in the PATH.
- *
- * @args: The array of command and arguments.
- */
-void execute_command(char **args)
-{
-    if (check_command_exists(args[0]))
-    {
-        char *full_path = get_command_path(args[0]);
-        if (full_path != NULL)
-        {
-            pid_t pid = fork();
-            if (pid == -1)
-            {
-                perror("fork error");
-                exit(EXIT_FAILURE);
-            }
-            else if (pid == 0)
-            {
-                /* Child process */
-                if (execve(full_path, args, NULL) == -1)
-                {
-                    perror("execve error");
-                    exit(EXIT_FAILURE);
-                }
-            }
-            else
-            {
-                /* Parent process */
-                int status;
-                waitpid(pid, &status, 0);
-            }
-            free(full_path);
-        }
-        else
-        {
-            fprintf(stderr, "%s: command not found\n", args[0]);
-        }
-    }
-    else
-    {
-        fprintf(stderr, "%s: command not found\n", args[0]);
-    }
-}
-
-/**
- * get_command_path - Retrieves the full path of a command from PATH.
- *
- * @command: The command to search for in PATH.
- *
- * Return: Returns the full path of the command if found in PATH, NULL otherwise.
- */
-char *get_command_path(const char *command)
-{
-    char *path = getenv("PATH");
-    if (path == NULL)
-    {
-        fprintf(stderr, "Unable to get PATH\n");
-        exit(EXIT_FAILURE);
-    }
-
-    char *path_copy = strdup(path);
-    char *token = strtok(path_copy, ":");
-
-    while (token != NULL)
-    {
-        char *full_path = (char *)malloc(strlen(token) + strlen(command) + 2);
-        if (full_path == NULL)
-        {
-            perror("malloc error");
-            exit(EXIT_FAILURE);
-        }
-        sprintf(full_path, "%s/%s", token, command);
-
-        if (access(full_path, X_OK) == 0)
-        {
-            free(path_copy);
-            return full_path;
-        }
-
-        free(full_path);
-        token = strtok(NULL, ":");
-    }
-
-    free(path_copy);
-    return NULL;
+    return strdup(input);
 }
 
 /**
  * main - Main function of the shell.
  *
- * @argc: The number of arguments passed to the program.
- * @argv: An array of pointers to the arguments.
- *
  * Return: Returns EXIT_SUCCESS upon successful execution.
  */
-int main(int argc __attribute__((unused)), char *argv[] __attribute__((unused)))
+int main()
 {
     char *command;
-    char **args;
-    size_t bufsize = BUFFER_SIZE;
 
-    if (isatty(STDIN_FILENO)) /* Check if in interactive mode */
-    {
-        do
-        {
-            display_prompt();
-            command = read_command();
+    do {
+        command = read_command();
 
-            if (strcmp(command, "") == 0)
-            {
-                free(command);
-                continue;
-            }
-
-            args = parse_arguments(command);
-
-            if (args != NULL && args[0] != NULL)
-            {
-                execute_command(args);
-                free(args);
-            }
-
+        if (feof(stdin)) {
             free(command);
-
-        } while (strcmp(command, "exit") != 0);
-    }
-    else /* Non-interactive mode */
-    {
-        command = (char *)malloc(bufsize * sizeof(char));
-        if (command == NULL)
-        {
-            perror("malloc error");
-            exit(EXIT_FAILURE);
+            write(STDOUT_FILENO, "\n", 1);
+            exit(EXIT_SUCCESS);
         }
 
-       
+        if (execute_command(command) == EXIT_FAILURE) {
+            free(command);
+            continue;
+        }
+
+        free(command);
+    } while (1);
+
+    return EXIT_SUCCESS;
+}
