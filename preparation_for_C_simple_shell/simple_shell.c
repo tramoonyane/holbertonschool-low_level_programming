8b9e27144894091ac00d62faa7487bccec6db11b
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-12-18 18:59:33 +0200

    Update simple_shell.c

--- preparation_for_C_simple_shell/simple_shell.c
+++ preparation_for_C_simple_shell/simple_shell.c
@@ -3,28 +3,52 @@
 #include "Simple_Shell.h"
 
 /**
- * execute_command - Executes the command provided by the user.
+ * execute_command - Executes the command with arguments.
  *
  * @command: The command to execute.
+ * @command_number: The number of the command.
+ * @program_name: The name of the program.
  *
  * Return: Returns EXIT_SUCCESS upon successful execution.
  */
-int execute_command(char *command) {
+int execute_command(char *command, int command_number, char *program_name) {
     pid_t pid;
     int status;
+    char **args;
+    char *p;
+    int arg_count = 1;  /* Initial count for command itself */
 
-    if (command == NULL || *command == '\0') {
-        return EXIT_SUCCESS; /* Skip execution for empty commands */
+    /* Count the number of arguments (tokens) */
+    for (p = command; *p != '\0'; ++p) {
+        if (*p == ' ') {
+            arg_count++;
+            while (*p == ' ')  /* Skip consecutive spaces */
+                p++;
+        }
     }
 
+    /* Allocate memory for the args array */
+    args = malloc((arg_count + 1) * sizeof(char *));
+    if (args == NULL) {
+        perror("malloc error");
+        exit(EXIT_FAILURE);
+    }
+
+    arg_count = 0;
+    args[arg_count++] = strtok(command, " \n");  /* Get the command */
+
+    /* Get the arguments and store them in the args array */
+    while ((args[arg_count++] = strtok(NULL, " \n")) != NULL);
+
     pid = fork();
+
     if (pid == -1) {
         perror("fork error");
         exit(EXIT_FAILURE);
     } else if (pid == 0) {
         /* Child process */
-        if (execlp(command, command, NULL) == -1) {
-            fprintf(stderr, "%s: command not found\n", command);
+        if (execvp(args[0], args) == -1) {
+            fprintf(stderr, "%s: %d: %s: command not found\n", program_name, command_number, args[0]);
             exit(EXIT_FAILURE);
         }
     } else {
@@ -32,6 +56,7 @@ int execute_command(char *command) {
         waitpid(pid, &status, 0);
     }
 
+    free(args);
     return EXIT_SUCCESS;
 }
 
@@ -43,8 +68,6 @@ int execute_command(char *command) {
 char* read_command() {
     char* command;
     char input[BUFFER_SIZE];
-
-    printf("%s", PROMPT);
 
     if (fgets(input, BUFFER_SIZE, stdin) == NULL) {
         if (feof(stdin)) {
@@ -74,23 +97,58 @@ char* read_command() {
  */
 int main() {
     char *command;
+    int command_number = 1;
+    char *program_name = "hsh"; /* Replace this with your program's name */
 
-    do {
-        command = read_command();
+    /* Check if input is from terminal or redirected from file/pipe */
+    if (isatty(STDIN_FILENO)) {
+        /* Interactive mode */
+        do {
+            printf("%s", PROMPT);
+            fflush(stdout); /* Flush stdout to ensure prompt is printed immediately */
+            
+            command = read_command();
 
-        if (feof(stdin)) {
+            if (feof(stdin)) {
+                free(command);
+                printf("\n");  /* Print newline for proper formatting */
+                exit(EXIT_SUCCESS);
+            }
+
+            if (execute_command(command, command_number, program_name) == EXIT_FAILURE) {
+                free(command);
+                continue;
+            }
+
             free(command);
-            write(STDOUT_FILENO, "\n", 1);
-            exit(EXIT_SUCCESS);
-        }
+            command_number++; /* Increment command number for each command */
+        } while (1);
+    } else {
+        /* Non-interactive mode */
+        char input[BUFFER_SIZE];
 
-        if (execute_command(command) == EXIT_FAILURE) {
-            free(command);
-            continue;
-        }
+        /* Process the command in the non-interactive mode */
+        while (fgets(input, BUFFER_SIZE, stdin)) {
+            /* Remove the newline character from input, if any */
+            input[strcspn(input, "\n")] = '\0';
 
-        free(command);
-    } while (1);
+            /* Execute the command */
+            if (execute_command(input, command_number, program_name) == EXIT_FAILURE) {
+                /* Handle error if needed */
+                /* Display error messages or perform necessary actions */
+                fprintf(stderr, "%s: %d: %s: command not found\n", program_name, command_number, input);
+            }
+
+            if (feof(stdin)) {
+                printf("\n"); /* Print newline for proper formatting before exiting */
+                exit(EXIT_SUCCESS);
+            } else {
+                printf("\n"); /* Print newline for proper formatting between commands */
+            }
+
+            command_number++; /* Increment command number for each command */
+        }
+    }
 
     return EXIT_SUCCESS;
 }
