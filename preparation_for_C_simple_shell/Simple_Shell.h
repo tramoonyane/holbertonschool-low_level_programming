dea618acae0be8351424186a7ba5bf8608f08ca8
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-12-19 22:00:59 +0200

    Update Simple_Shell.h

--- preparation_for_C_simple_shell/Simple_Shell.h
+++ preparation_for_C_simple_shell/Simple_Shell.h
@@ -12,8 +12,13 @@
 
 #define MAX_INPUT_LENGTH 1024
 #define MAX_TOKENS 100
+#define BUFFER_SIZE 1024
+#define PROMPT ":) "
 
 extern char **environ;  /* Access to the environment variables */
+
+/* Function to check if input is coming from a terminal */
+int isatty(int fd);
 
 /* Function to check if input is coming from a terminal */
 int is_input_terminal();
@@ -23,8 +28,12 @@ void execute_interactively(char *program_name);
 void execute_non_interactively(char *program_name);
 char *get_user_input();
 char **tokenize_input(char *input);
+int execute_command(char *command, int command_number, char *program_name);
 void execute_command(char **tokens, char *program_name);
 char **parse_arguments(char *input);
 void execute_with_arguments(char **tokens, char *program_name);
+char **parse_path();
+char *read_command();
+int handle_builtin_commands(char *command);
 
 #endif /* SIMPLE_SHELL_H */
