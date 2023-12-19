001332d2e4a35de3a3bb00215f046d7b62f699aa
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-12-19 17:10:49 +0200

    Update simple_shell_arguments.c

--- preparation_for_C_simple_shell/simple_shell_arguments.c
+++ preparation_for_C_simple_shell/simple_shell_arguments.c
@@ -42,37 +42,75 @@ return (tokens);
  */
 void execute_with_arguments(char **tokens, char *program_name)
 {
-pid_t child_pid;
-int status;
+    pid_t child_pid;
+    int status;
+    int found = 0;
+    
+    child_pid = fork();
+    if (child_pid < 0)
+    {
+        perror("Fork failed");
+        exit(EXIT_FAILURE);
+    }
+    else if (child_pid == 0)
+    {
+        /* Check if the command exists in PATH */
+        char *path = getenv("PATH");
+        char *path_copy = strdup(path);
+        char *dir = strtok(path_copy, ":");
+        while (dir != NULL)
+        {
+            char *command_path = malloc(strlen(dir) + strlen(tokens[0]) + 2);
+            sprintf(command_path, "%s/%s", dir, tokens[0]);
+            if (access(command_path, F_OK) == 0)
+            {
+                found = 1;
+                if (execve(command_path, tokens, environ) == -1)
+                {
+                    fprintf(stderr, "%s: ", program_name);
+                    perror("Command execution failed");
+                    exit(EXIT_FAILURE);
+                }
+            }
+            free(command_path);
+            dir = strtok(NULL, ":");
+        }
+        free(path_copy);
+        
+        /* Command not found in PATH */
+        if (!found)
+        {
+            fprintf(stderr, "%s: command not found\n", tokens[0]);
+            exit(EXIT_FAILURE);
+        }
+    }
+    else
+    {
+        do {
+            waitpid(child_pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+}
 
-child_pid = fork();
-if (child_pid < 0)
+/**
+ * execute_env_command - Print the current environment variables.
+ *
+ * Description: This function prints the current environment variables.
+ * It accesses the global variable 'environ' containing the environment
+ * variable pointers and iterates through them to print each variable.
+ */
+void execute_env_command()
 {
-perror("Fork failed");
-exit(EXIT_FAILURE);
-}
-else if (child_pid == 0)
-{
-/* Check if the command exists in PATH */
-if (access(tokens[0], F_OK) == 0)
-{
-if (execve(tokens[0], tokens, environ) == -1)
-{
-fprintf(stderr, "%s: ", program_name);
-perror("Command execution failed");
-exit(EXIT_FAILURE);
-}
-}
-else
-{
-fprintf(stderr, "%s: command not found\n", tokens[0]);
-exit(EXIT_FAILURE);
-}
-}
-else
-{
-do {
-waitpid(child_pid, &status, WUNTRACED);
-} while (!WIFEXITED(status) && !WIFSIGNALED(status));
-}
+    extern char **environ; /* External environment variable list */
+    char **env = environ;  /* Pointer to environment variables */
+
+    /*
+     * Iterate through the environment variables and print each variable.
+     * Prints each environment variable followed by a newline character.
+     */
+    while (*env)
+    {
+        printf("%s\n", *env);
+        env++;
+    }
 }
