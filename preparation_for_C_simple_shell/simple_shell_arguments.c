84fbb49071909238f8c3040eaaaea4fc6f93eb4e
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-12-19 23:08:05 +0200

    Update simple_shell_arguments.c

--- preparation_for_C_simple_shell/simple_shell_arguments.c
+++ preparation_for_C_simple_shell/simple_shell_arguments.c
@@ -159,6 +159,24 @@ char* read_command()
 }
 
 /**
+ * handle_builtin_commands - Handles built-in commands like 'exit' and 'env'.
+ *
+ * @command: The command to execute.
+ *
+ * Return: Returns 1 if a built-in command is executed, otherwise 0.
+ */
+int handle_builtin_commands(char *command) {
+    if (strcmp(command, "exit") == 0) {
+        exit_shell(); /* Call the exit_shell function */
+        return 1;     /* Return 1 to indicate the command was handled */
+    } else if (strcmp(command, "env") == 0) {
+        print_environment(); /* Print the environment variables */
+        return 1;            /* Return 1 to indicate the command was handled */
+    }
+    return 0; /* Return 0 for other commands */
+}
+
+/**
  * main - Main function of the shell.
  *
  * Return: Returns EXIT_SUCCESS upon successful execution.
