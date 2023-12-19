34ffacc851b19984893af6b607cee54d44c236c8
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-12-19 19:50:56 +0200

    Update simple_shell_arguments.c

--- preparation_for_C_simple_shell/simple_shell_arguments.c
+++ preparation_for_C_simple_shell/simple_shell_arguments.c
@@ -3,110 +3,137 @@
 #include "Simple_Shell.h"
 
 /**
- * parse_arguments - Parses command line arguments.
- * @input: The input string containing command and arguments.
+ * main - Entry point of the simple shell program
+ * @argc: The number of arguments.
+ * @argv: Array of arguments passed to the program.
+ *
+ * Return: Always 0 upon successful execution.
+ *
+ * Description: This function acts as the entry point
+ * of the simple shell program.
+ * It determines if the input is from a terminal
+ * or not and executes accordingly.
+ */
+int main(int argc, char *argv[])
+{
+char *program_name = argv[0]; /* Set the program name from argv[0] */
+(void)argc;
+if (is_input_terminal())
+{
+execute_interactively(program_name);
+}
+else
+{
+execute_non_interactively(program_name);
+}
+return ((EXIT_SUCCESS));
+}
+
+/**
+ * execute_interactively - Executes commands interactively from the terminal
+ * @program_name: The name of the program.
+ *
+ * Description: This function handles the interactive mode of the shell.
+ * It displays a prompt and waits for user input to execute commands.
+ */
+void execute_interactively(char *program_name)
+{
+char *input;
+
+while (1)
+{
+printf("$ "); /* Display prompt only in interactive mode */
+fflush(stdout);
+input = get_user_input();
+if (input == NULL)
+{
+printf("\n"); /* Print newline after Ctrl+D */
+break;        /* Exit on EOF */
+}
+execute_command(tokenize_input(input), program_name);
+}
+}
+
+/**
+ * execute_non_interactively - Executes commands non-interactively
+ * @program_name: The name of the program.
+ *
+ * Description: This function handles the non-interactive mode of the shell.
+ * It reads input from pipes or redirected input to execute commands.
+ */
+void execute_non_interactively(char *program_name)
+{
+char *input_from_pipe = get_user_input();
+if (input_from_pipe != NULL)
+{
+execute_command(tokenize_input(input_from_pipe), program_name);
+}
+}
+
+/**
+ * tokenize_input - Tokenizes the input string into individual tokens
+ * @input: The input string to tokenize.
  *
  * Return: A pointer to an array of tokens (strings).
  *
  * Description: This function tokenizes the input string
- * based on space delimiter and returns an array of tokens
- * obtained from the input (command and arguments).
+ * based on space delimiter
+ * and returns an array of tokens (strings) obtained from the input.
  */
-char **parse_arguments(char *input) {
-    int i = 0;
-    char **tokens = malloc(MAX_TOKENS * sizeof(char *));
-    if (tokens == NULL) {
-        perror("Memory allocation failed");
-        exit(EXIT_FAILURE);
-    }
-    tokens[i] = strtok(input, " "); /* Tokenize input */
-    while (tokens[i] != NULL && i < MAX_TOKENS - 1) {
-        i++;
-        tokens[i] = strtok(NULL, " ");
-    }
-    tokens[i + 1] = NULL;
-    return tokens;
+char **tokenize_input(char *input)
+{
+int i = 0;
+char **tokens = malloc(MAX_TOKENS * sizeof(char *));
+
+if (tokens == NULL)
+{
+perror("Memory allocation failed");
+exit(EXIT_FAILURE);
+}
+tokens[i] = strtok(input, " "); /* Tokenize input */
+while (tokens[i] != NULL && i < MAX_TOKENS - 1)
+{
+i++;
+tokens[i] = strtok(NULL, " ");
+}
+tokens[i + 1] = NULL;
+return (tokens);
 }
 
 /**
- * execute_env_command - Print the current environment variables.
- *
- * Description: This function prints the current environment variables.
- * It accesses the global variable 'environ' containing the environment
- * variable pointers and iterates through them to print each variable.
- */
-void execute_env_command() {
-    char *env_cmd = "/bin/env";
-
-    pid_t child_pid;
-    int status;
-
-    printf("Executing command: %s\n", env_cmd);
-
-    child_pid = fork();
-    if (child_pid < 0) {
-        perror("Fork failed");
-        exit(EXIT_FAILURE);
-    } else if (child_pid == 0) {
-        char **env_argv = malloc(2 * sizeof(char *));
-        if (env_argv == NULL) {
-            perror("Memory allocation failed");
-            exit(EXIT_FAILURE);
-        }
-        
-        env_argv[0] = env_cmd;
-        env_argv[1] = NULL;
-        
-        if (execve(env_cmd, env_argv, environ) == -1) {
-            fprintf(stderr, "Error executing command: %s\n", env_cmd);
-            perror("Command execution failed");
-            exit(EXIT_FAILURE);
-        }
-        
-        free(env_argv); /* Free allocated memory */
-    } else {
-        do {
-            waitpid(child_pid, &status, WUNTRACED);
-        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
-    }
-}
-
-/**
- * execute_with_arguments - Executes a command with arguments using execve.
- * @tokens: An array of command tokens (command and arguments).
+ * execute_command - Executes a command using execve
+ * @tokens: An array of command tokens.
  * @program_name: The name of the program.
  *
- * Description: This function executes a command with arguments
- * using the execve system call with the given tokens and program name.
+ * Description: This function executes a command
+ * using the execve system call
+ * with the given tokens and program name.
  * It forks a child process to execute the command.
  */
-void execute_with_arguments(char **tokens, char *program_name) {
-    pid_t child_pid;
-    int status;
+void execute_command(char **tokens, char *program_name)
+{
+pid_t child_pid;
+int status;
 
-    printf("Command: %s\n", tokens[0]); /* Debug print */
-
-    child_pid = fork();
-    if (child_pid < 0) {
-        perror("Fork failed");
-        exit(EXIT_FAILURE);
-    } else if (child_pid == 0) {
-        if (strcmp(tokens[0], "env") == 0) {
-            execute_env_command(); /* Call the function to handle 'env' command */
-            exit(EXIT_SUCCESS); /* Exit the child process after executing 'env' */
-        } else if (access(tokens[0], F_OK) == 0) {
-            if (execve(tokens[0], tokens, environ) == -1) {
-                fprintf(stderr, "%s: ", program_name);
-                perror("Command execution failed");
-                exit(EXIT_FAILURE);
-            }
-        } else {
-            fprintf(stderr, "%s: command not found\n", tokens[0]);
-            exit(EXIT_FAILURE);
-        }
-    } else {
-        do {
-            waitpid(child_pid, &status, WUNTRACED);
-        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
-    }
+child_pid = fork();
+if (child_pid < 0)
+{
+perror("Fork failed");
+exit(EXIT_FAILURE);
+}
+else if (child_pid == 0)
+{
+if (execve(tokens[0], tokens, environ) == -1)
+{
+fprintf(stderr, "%s: ", program_name);
+perror("Command execution failed");
+exit(EXIT_FAILURE);
+}
+}
+else
+{
+do {
+waitpid(child_pid, &status, WUNTRACED);
+} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+}
 }
