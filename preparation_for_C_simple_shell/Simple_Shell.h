fd56654cae32a9ff8e6c74640827bfb3746c91d5
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-12-19 03:00:19 +0200

    Update Simple_Shell.h

--- preparation_for_C_simple_shell/Simple_Shell.h
+++ preparation_for_C_simple_shell/Simple_Shell.h
@@ -1,33 +1,14 @@
-/* Simple_Shell.h */
-
 #ifndef SIMPLE_SHELL_H
 #define SIMPLE_SHELL_H
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
-#include <limits.h>
-#include <sys/types.h>
 #include <sys/wait.h>
-#include <libgen.h>  /* Include the library to use basename() */
+#include <string.h>
 
-#define BUFFER_SIZE 1024
-#define PROMPT "$ "
+#define MAX_COMMAND_LENGTH 100
 
-/* Reads a command from standard input */
-char *read_command();
-
-/* Executes the command with arguments */
-int execute_command(char *command, int command_number, char *program_name);
-
-/* Tokenizes the PATH environment variable */
-char **parse_path();
-
-/* Handles built-in commands like 'exit' */
-int handle_builtin_commands(char *command);
-
-/* Main function of the shell */
-int main(void);
+void execute_command(char *command);
 
 #endif /* SIMPLE_SHELL_H */
