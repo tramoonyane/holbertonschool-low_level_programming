9cfe0cff99e564a320e17a9cf3b2bf98797930fe
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-12-19 22:56:53 +0200

    Update Shell.h

---
+++ preparation_for_C_simple_shell/Shell.h
@@ -0,0 +1,21 @@
+/* Simple_Shell.h */
+
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define BUFFER_SIZE 1024
+#define PROMPT "$ " /* You can change the prompt as needed */
+
+char **parse_path();
+int execute_command(char *command, int command_number, char *program_name);
+char* read_command();
+int handle_builtin_commands(char *command);
+
+#endif /* SHELL_H */
