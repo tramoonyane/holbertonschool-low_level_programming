4e91acd1b6f9beee2349e0ee8ecd0f23d3ca6131
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-12-19 15:03:30 +0200

    Update helper_file1.c

--- preparation_for_C_simple_shell/helper_file1.c
+++ preparation_for_C_simple_shell/helper_file1.c
@@ -1,3 +1,6 @@
+/* helper_file1.c */
+
+#include "Simple_Shell.h"
 
 /**
  * is_input_terminal - Checks if input is coming from a terminal
@@ -8,3 +11,25 @@ int is_input_terminal(void)
 {
 return (isatty(fileno((stdin))));
 }
+
+/**
+ * get_user_input - Retrieves user input from stdin
+ *
+ * Return: A pointer to the user input string.
+ *
+ * Description: This function reads user input
+ * from standard input (stdin)
+ * and returns the input as a string,
+ * removing the newline character if present.
+ */
+char *get_user_input()
+{
+char input[MAX_INPUT_LENGTH];
+if (fgets(input, sizeof(input), stdin) != NULL)
+{
+input[strcspn(input, "\n")] = '\0'; /* Remove newline character */
+return (strdup(input));
+}
+return (NULL);
+}
+
