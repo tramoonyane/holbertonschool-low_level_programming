1e3a126c7526b300255cf9634dae0666c4388433
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-12-19 04:04:43 +0200

    Update simple_shell.c

--- preparation_for_C_simple_shell/simple_shell.c
+++ preparation_for_C_simple_shell/simple_shell.c
@@ -1,4 +1,3 @@
-/* simple_shell.c */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,6 +6,9 @@
 #include <sys/wait.h>
 
 #define MAX_INPUT_LENGTH 1024
+#define MAX_TOKENS 100
+
+extern char **environ;  // Access to the environment variables
 
 /**
  * main - Simple Shell
@@ -15,60 +17,61 @@
  */
 int main(void)
 {
-	char input[MAX_INPUT_LENGTH];
-	char *command;
-	pid_t child_pid;
-	int status;
+    char input[MAX_INPUT_LENGTH];
+    char *tokens[MAX_TOKENS];
+    pid_t child_pid;
+    int status;
 
-	while (1)
-	{
-		printf("$ "); /* Display prompt */
-		fflush(stdout);
+    while (1)
+    {
+        printf("$ "); /* Display prompt */
+        fflush(stdout);
 
-		if (fgets(input, sizeof(input), stdin) == NULL)
-		{
-			if (feof(stdin))
-			{
-				printf("\n"); /* Print newline after Ctrl+D */
-				break; /* Exit on EOF */
-			}
-		}
+        if (fgets(input, sizeof(input), stdin) == NULL)
+        {
+            if (feof(stdin))
+            {
+                printf("\n"); /* Print newline after Ctrl+D */
+                break; /* Exit on EOF */
+            }
+        }
 
-		input[strcspn(input, "\n")] = '\0'; /* Remove newline character */
+        input[strcspn(input, "\n")] = '\0'; /* Remove newline character */
 
-		child_pid = fork();
+        child_pid = fork();
 
-		if (child_pid < 0)
-		{
-			perror("Fork failed");
-			exit(EXIT_FAILURE);
-		}
-		else if (child_pid == 0)
-		{
-			command = strtok(input, " "); /* Tokenize input */
+        if (child_pid < 0)
+        {
+            perror("Fork failed");
+            exit(EXIT_FAILURE);
+        }
+        else if (child_pid == 0)
+        {
+            int i = 0;
+            tokens[i] = strtok(input, " "); /* Tokenize input */
 
-			if (command != NULL)
-			{
-				if (execve(command, &command, NULL) == -1)
-				{
-					perror("Command execution failed");
-					exit(EXIT_FAILURE);
-				}
-			}
-			else
-			{
-				printf("Please enter a command.\n");
-				exit(EXIT_FAILURE);
-			}
-		}
-		else
-		{
-			do
-			{
-				waitpid(child_pid, &status, WUNTRACED);
-			} while (!WIFEXITED(status) && !WIFSIGNALED(status));
-		}
-	}
+            while (tokens[i] != NULL && i < MAX_TOKENS - 1)
+            {
+                i++;
+                tokens[i] = strtok(NULL, " ");
+            }
 
-	return (EXIT_SUCCESS);
+            tokens[i + 1] = NULL;
+
+            if (execve(tokens[0], tokens, environ) == -1)
+            {
+                perror("Command execution failed");
+                exit(EXIT_FAILURE);
+            }
+        }
+        else
+        {
+            do
+            {
+                waitpid(child_pid, &status, WUNTRACED);
+            } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+        }
+    }
+
+    return EXIT_SUCCESS;
 }
