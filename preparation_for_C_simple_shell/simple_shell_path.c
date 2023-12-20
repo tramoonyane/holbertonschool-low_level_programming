7b811a3d30f8f455af84b5f16a1117e44a8d7634
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-12-20 04:14:16 +0200

    Update simple_shell_path.c

--- preparation_for_C_simple_shell/simple_shell_path.c
+++ preparation_for_C_simple_shell/simple_shell_path.c
@@ -3,57 +3,87 @@
 #include "Shell.h"
 
 /**
- * parse_path - Tokenizes the PATH environment variable.
+ * parse_path - Tokenizes the PATH environment variable into directories.
  *
  * Return: Returns an array of directories.
  */
-char **parse_path()
+char **parse_path(void)
 {
-char *path = getenv("PATH");
-char *token;
-int count;
-char *path_copy;
-char **directories = malloc(sizeof(char *));
+    char *path = get_path();
+    char *path_copy = strdup(path);
+    char **directories = allocate_directories(1);
 
-if (path == NULL || *path == '\0')
-{
-fprintf(stderr, "No PATH variable found or empty.\n");
-exit(EXIT_FAILURE);
+    char *token = strtok(path_copy, ":");
+    int count = 0;
+
+    while (token != NULL)
+    {
+        add_directory(&directories, token, count++);
+        token = strtok(NULL, ":");
+    }
+
+    directories = realloc(directories, (count + 1) * sizeof(char *));
+    if (directories == NULL)
+    {
+        perror("realloc error");
+        exit(EXIT_FAILURE);
+    }
+    directories[count] = NULL;
+
+    free(path_copy);
+    return directories;
 }
-path_copy = strdup(path);
-if (path_copy == NULL)
+
+/**
+ * add_directory - Adds a directory to the directories array.
+ *
+ * @directories: Pointer to the array of directories.
+ * @token: The directory string to be added.
+ * @count: Index of the directory being added.
+ */
+void add_directory(char ***directories, char *token, int count)
 {
-perror("strdup error");
-exit(EXIT_FAILURE);
+    *directories = realloc(*directories, (count + 1) * sizeof(char *));
+    if (*directories == NULL)
+    {
+        perror("realloc error");
+        exit(EXIT_FAILURE);
+    }
+    (*directories)[count] = token;
 }
-token = strtok(path_copy, ":");
-count = 0;
-directories = malloc(sizeof(char *));
-if (directories == NULL)
+
+/**
+ * allocate_directories - Allocates memory for the directories array.
+ *
+ * @size: Size of the array to be allocated.
+ * Return: Pointer to the newly allocated array of directories.
+ */
+char **allocate_directories(int size)
 {
-perror("malloc error");
-exit(EXIT_FAILURE);
+    char **directories = malloc(size * sizeof(char *));
+    if (directories == NULL)
+    {
+        perror("malloc error");
+        exit(EXIT_FAILURE);
+    }
+    return directories;
 }
-while (token != NULL)
+
+/**
+ * get_path - Retrieves the PATH environment variable.
+ *
+ * Return: Pointer to the PATH environment variable string.
+ */
+char *get_path(void)
 {
-directories = realloc(directories, (count + 1) * sizeof(char *));
-if (directories == NULL)
-{
-perror("realloc error");
-exit(EXIT_FAILURE);
-}
-directories[count++] = token;
-token = strtok(NULL, ":");
-}
-directories = realloc(directories, (count + 1) * sizeof(char *));
-if (directories == NULL)
-{
-perror("realloc error");
-exit(EXIT_FAILURE);
-}
-directories[count] = NULL;
-free(path_copy);
-return (directories);
+    char *path = getenv("PATH");
+
+    if (path == NULL || *path == '\0')
+    {
+        fprintf(stderr, "No PATH variable found or empty.\n");
+        exit(EXIT_FAILURE);
+    }
+    return path;
 }
 
 /**
