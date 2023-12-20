b5f06c9380bfcbe9652710ce9e2b5283c59cc089
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-12-20 13:50:27 +0200

    Update Shell.h

--- preparation_for_C_simple_shell/Shell.h
+++ preparation_for_C_simple_shell/Shell.h
@@ -32,5 +32,6 @@ char *get_path(void);
 char **allocate_directories(int size);
 void add_directory(char ***directories, char *token, int count);
 
+void handle_command_not_found(int found, const char *command, int command_number, const char *program_name);
 
 #endif /* SHELL_H */
