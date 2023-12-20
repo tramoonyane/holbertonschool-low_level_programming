138119e69d4d99f596929cec6de36bddfef74860
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-12-20 02:10:32 +0200

    Update helper_file2.c

--- preparation_for_C_simple_shell/helper_file2.c
+++ preparation_for_C_simple_shell/helper_file2.c
@@ -5,13 +5,15 @@
 /**
  * print_environment - Prints the current environment variables.
  */
-void print_environment() {
-    extern char **environ;
-    int i = 0;
-    while (environ[i] != NULL) {
-        printf("%s\n", environ[i]);
-        i++;
-    }
+void print_environment()
+{
+extern char **environ;
+int i = 0;
+while (environ[i] != NULL)
+{
+printf("%s\n", environ[i]);
+i++;
+}
 }
 
 /**
@@ -21,28 +23,30 @@ void print_environment() {
  */
 char* read_command()
 {
-    char* command;
-    char input[BUFFER_SIZE];
+char* command;
+char input[BUFFER_SIZE];
 
-    if (fgets(input, BUFFER_SIZE, stdin) == NULL) {
-        if (feof(stdin)) {
-            write(STDOUT_FILENO, "\n", 1);
-            exit(EXIT_SUCCESS);
-        } else {
-            perror("fgets error");
-            exit(EXIT_FAILURE);
-        }
-    }
-
-    input[strcspn(input, "\n")] = '\0';
-
-    command = strdup(input);
-    if (command == NULL) {
-        perror("strdup error");
-        exit(EXIT_FAILURE);
-    }
-
-    return command;
+if (fgets(input, BUFFER_SIZE, stdin) == NULL)
+{
+if (feof(stdin))
+{
+write(STDOUT_FILENO, "\n", 1);
+exit(EXIT_SUCCESS);
+}
+else
+{
+perror("fgets error");
+exit(EXIT_FAILURE);
+}
+}
+input[strcspn(input, "\n")] = '\0';
+command = strdup(input);
+if (command == NULL)
+{
+perror("strdup error");
+exit(EXIT_FAILURE);
+}
+return (command);
 }
 
 /**
@@ -52,22 +56,26 @@ char* read_command()
  *
  * Return: Returns 1 if a built-in command is executed, otherwise 0.
  */
-int handle_builtin_commands(char *command) {
-    if (strcmp(command, "exit") == 0) {
-        exit_shell(); /* Call the exit_shell function */
-        return 1;     /* Return 1 to indicate the command was handled */
-    } else if (strcmp(command, "env") == 0) {
-        print_environment(); /* Print the environment variables */
-        return 1;            /* Return 1 to indicate the command was handled */
-    }
-    return 0; /* Return 0 for other commands */
+int handle_builtin_commands(char *command)
+{
+if (strcmp(command, "exit") == 0)
+{
+exit_shell(); /* Call the exit_shell function */
+return (1);     /* Return 1 to indicate the command was handled */
+}
+else if (strcmp(command, "env") == 0)
+{
+print_environment(); /* Print the environment variables */
+return (1);            /* Return 1 to indicate the command was handled */
+}
+return (0); /* Return 0 for other commands */
 }
 
 
 /**
  * exit_shell - Exits the shell.
  */
-void exit_shell() {
-    exit(EXIT_SUCCESS);
+void exit_shell()
+{
+exit(EXIT_SUCCESS);
 }
-
