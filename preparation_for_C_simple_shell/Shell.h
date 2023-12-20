ea1a2d4274197d6cab68ac98b0122e0660b0ff94
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-12-20 16:51:52 +0200

    Update Shell.h

--- preparation_for_C_simple_shell/Shell.h
+++ preparation_for_C_simple_shell/Shell.h
@@ -1,5 +1,3 @@
-/* Shell.h */
-
 #ifndef SHELL_H
 #define SHELL_H
 
@@ -24,16 +22,19 @@ void interactive_mode(char *program_name);
 void non_interactive_mode(char *program_name);
 void process_command(char *command, int *command_number, char *program_name);
 
-void execute_found_command(char **args, int command_number, char *program_name, const char *path_command);
+void execute_found_command(char **args, int command_number, char *program_name,
+const char *path_command);
 
 int count_arguments(char *command);
 char **get_command_arguments(char *command, int arg_count);
-void execute_command_with_path(char **args, int command_number, char *program_name);
+void execute_command_with_path(char **args, int command_number,
+char *program_name);
 
 char *get_path(void);
 char **allocate_directories(int size);
 void add_directory(char ***directories, char *token, int count);
 
-void handle_command_not_found(int found, const char *command, int command_number, const char *program_name);
+void handle_command_not_found(int found, const char *command, int command_number,
+const char *program_name);
 
 #endif /* SHELL_H */
