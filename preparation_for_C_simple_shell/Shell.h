012e6a4e51c2ddb506896b2327cae562945e7932
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-12-20 13:47:34 +0200

    Update Shell.h

--- preparation_for_C_simple_shell/Shell.h
+++ preparation_for_C_simple_shell/Shell.h
@@ -32,10 +32,5 @@ char *get_path(void);
 char **allocate_directories(int size);
 void add_directory(char ***directories, char *token, int count);
 
-int search_and_execute_command(char **args, int command_number, char *program_name, char **directories);
-void handle_command_not_found(int found, const char *command, int command_number, const char *program_name);
-
-void construct_path_command(const char *directory, const char *command, char *path_command);
-int is_command_executable(const char *path_command);
 
 #endif /* SHELL_H */
