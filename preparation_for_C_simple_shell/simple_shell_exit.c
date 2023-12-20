66c44dcd9b4b37ba44445a8de3bd103befb5e0d1
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2023-12-20 15:19:29 +0200

    Update simple_shell_exit.c

--- preparation_for_C_simple_shell/simple_shell_exit.c
+++ preparation_for_C_simple_shell/simple_shell_exit.c
@@ -1,40 +1,218 @@
-/* simple_shell_exit.c */
+/* simple_shell_arguments.c */
 
-#include "Simple_Shell.h"
-
-/**
- * exit_shell - Exits the shell.
- */
-void exit_shell() {
-    exit(EXIT_SUCCESS);
-}
+#include "Shell.h"
 
 /**
- * print_environment - Prints the current environment variables.
+ * parse_path - Tokenizes the PATH environment variable into directories.
+ *
+ * Return: Returns an array of directories.
  */
-void print_environment() {
-    extern char **environ;
-    int i = 0;
-    while (environ[i] != NULL) {
-        printf("%s\n", environ[i]);
-        i++;
+char **parse_path(void)
+{
+    char *path = get_path();
+    char *path_copy = strdup(path);
+    char **directories = allocate_directories(1);
+
+    char *token = strtok(path_copy, ":");
+    int count = 0;
+
+    while (token != NULL)
+    {
+        add_directory(&directories, token, count++);
+        token = strtok(NULL, ":");
     }
+
+    directories = realloc(directories, (count + 1) * sizeof(char *));
+    if (directories == NULL)
+    {
+        perror("realloc error");
+        exit(EXIT_FAILURE);
+    }
+    directories[count] = NULL;
+
+    free(path_copy);
+    return directories;
 }
 
 /**
- * handle_builtin_commands - Handles built-in commands like 'exit' and 'env'.
+ * add_directory - Adds a directory to the directories array.
+ *
+ * @directories: Pointer to the array of directories.
+ * @token: The directory string to be added.
+ * @count: Index of the directory being added.
+ */
+void add_directory(char ***directories, char *token, int count)
+{
+    *directories = realloc(*directories, (count + 1) * sizeof(char *));
+    if (*directories == NULL)
+    {
+        perror("realloc error");
+        exit(EXIT_FAILURE);
+    }
+    (*directories)[count] = token;
+}
+
+/**
+ * allocate_directories - Allocates memory for the directories array.
+ *
+ * @size: Size of the array to be allocated.
+ * Return: Pointer to the newly allocated array of directories.
+ */
+char **allocate_directories(int size)
+{
+    char **directories = malloc(size * sizeof(char *));
+    if (directories == NULL)
+    {
+        perror("malloc error");
+        exit(EXIT_FAILURE);
+    }
+    return directories;
+}
+
+/**
+ * get_path - Retrieves the PATH environment variable.
+ *
+ * Return: Pointer to the PATH environment variable string.
+ */
+char *get_path(void)
+{
+    char *path = getenv("PATH");
+
+    if (path == NULL || *path == '\0')
+    {
+        fprintf(stderr, "No PATH variable found or empty.\n");
+        exit(EXIT_FAILURE);
+    }
+    return path;
+}
+
+/**
+ * execute_command - Executes the command with arguments.
  *
  * @command: The command to execute.
+ * @command_number: Number of the command in the shell session.
+ * @program_name: The name of the shell program.
  *
- * Return: Returns 1 if a built-in command is executed, otherwise 0.
+ * Return: Returns EXIT_SUCCESS upon successful execution.
  */
-int handle_builtin_commands(char *command) {
-    if (strcmp(command, "exit") == 0) {
-        exit_shell(); /* Call the exit_shell function */
-        return 1;     /* Return 1 to indicate the command was handled */
-    } else if (strcmp(command, "env") == 0) {
-        print_environment(); /* Print the environment variables */
-        return 1;            /* Return 1 to indicate the command was handled */
-    }
-    return 0; /* Return 0 for other commands */
+int execute_command(char *command, int command_number, char *program_name)
+{
+char **args;
+int arg_count;
+
+if (command == NULL || *command == '\0')
+{
+return (EXIT_SUCCESS); /* Skip execution for empty commands */
+}
+arg_count = count_arguments(command);
+args = get_command_arguments(command, arg_count);
+execute_command_with_path(args, command_number, program_name);
+free(args);
+return (EXIT_SUCCESS);
+}
+
+/**
+ * count_arguments - Counts the number of arguments in a command.
+ *
+ * @command: The command to analyze.
+ *
+ * Return: Returns the number of arguments.
+ */
+int count_arguments(char *command)
+{
+int arg_count = 1; /* Initial count for command itself */
+char *p;
+
+for (p = command; *p != '\0'; ++p)
+{
+if (*p == ' ')
+{
+arg_count++;
+while (*p == ' ') /* Skip consecutive spaces */
+p++;
+}
+}
+return (arg_count);
+}
+
+/**
+ * get_command_arguments - Splits the command into arguments.
+ *
+ * @command: The command to split.
+ * @arg_count: Number of arguments in the command.
+ *
+ * Return: Returns an array of command arguments.
+ */
+char **get_command_arguments(char *command, int arg_count)
+{
+char **args;
+int i = 0;
+
+args = malloc((arg_count + 1) * sizeof(char *));
+if (args == NULL)
+{
+perror("malloc error");
+exit(EXIT_FAILURE);
+}
+args[i++] = strtok(command, " \n"); /* Get the command */
+while
+((args[i++] = strtok(NULL, " \n")) != NULL);
+return (args);
+}
+
+/**
+ * execute_command_with_path - Executes the command with full path.
+ *
+ * @args: Array of command arguments.
+ * @command_number: Number of the command in the shell session.
+ * @program_name: The name of the shell program.
+ */
+void execute_command_with_path(char **args,
+int command_number, char *program_name)
+{
+pid_t pid;
+int status;
+char **directories = parse_path();
+int found = 0;
+int i;
+
+for (i = 0; directories[i] != NULL; i++)
+{
+char path_command[BUFFER_SIZE];
+
+snprintf(path_command, sizeof(path_command), "%s/%s", directories[i], args[0]);
+/* Check if the command is executable at the given path */
+if (access(path_command, X_OK) == 0)
+{
+found = 1;
+pid = fork();
+if (pid == -1)
+{
+perror("fork error");
+exit(EXIT_FAILURE);
+}
+else if (pid == 0)
+{
+/* Child process */
+if (execv(path_command, args) == -1)
+{
+fprintf(stderr, "%s: %d: %s: not found\n",
+program_name, command_number, args[0]);
+exit(EXIT_FAILURE);
+}
+}
+else
+{
+/* Parent process */
+waitpid(pid, &status, 0);
+}
+break;
+}
+}
+if (!found)
+{
+fprintf(stderr, "%s: %d: %s: not found\n",
+program_name, command_number, args[0]);
+}
+free(directories);
 }
