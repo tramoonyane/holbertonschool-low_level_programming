36c49ea5c43a73e0acf136f02319927a252bc042
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-01-17 14:58:19 +0200

    Update 1-binary.c

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -10,6 +10,7 @@
  * Return: If the value is not present or the array is NULL, -1.
  *         Otherwise, the index where the value is located.
  */
+
 int binary_search(int *array, size_t size, int value)
 {
 	size_t i, left, right;
